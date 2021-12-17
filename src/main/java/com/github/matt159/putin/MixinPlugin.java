d09d576ce37f0bcdf1c35fc2e0e8ccaee79d4fb6
matt-159 <16054364+matt-159@users.noreply.github.com>
2021-12-17 04:23:50 -0800

    shifted crafting matrix slots

--- src/main/java/com/github/matt159/putin/MixinPlugin.java
+++ src/main/java/com/github/matt159/putin/MixinPlugin.java
@@ -1,5 +1,6 @@
 package com.github.matt159.putin;
 
+import com.github.matt159.putin.mixins.ContainerPlayerMixin;
 import com.google.common.collect.Lists;
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
@@ -30,7 +31,7 @@ public class MixinPlugin implements IMixinConfigPlugin {
     @Override
     public List<String> getMixins() {
         return Lists.newArrayList(
-                "GuiContainerMixin"
+                "ContainerPlayerMixin"
         );
     }
 
---
+++ src/main/java/com/github/matt159/putin/mixins/ContainerPlayerMixin.java
@@ -0,0 +1,28 @@
+package com.github.matt159.putin.mixins;
+
+import net.minecraft.inventory.ContainerPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerPlayer.class)
+public abstract class ContainerPlayerMixin {
+    private static final int CRAFT_MATRIX_X_OFFSET = 250;
+    private static final int CRAFT_RESULT_X_OFFSET = 306;
+
+    @ModifyConstant(method="<init>",
+                    constant = @Constant(intValue = 88,
+                                        ordinal = 0),
+                    require = 1)
+    private int shiftCraftingMatrix(int value) {
+        return CRAFT_MATRIX_X_OFFSET;
+    }
+
+    @ModifyConstant(method="<init>",
+            constant = @Constant(intValue = 144,
+                    ordinal = 0),
+            require = 1)
+    private int shiftResultMatrix(int value) {
+        return CRAFT_RESULT_X_OFFSET;
+    }
+}
