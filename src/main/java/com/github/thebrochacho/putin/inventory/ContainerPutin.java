0b8e71a0959f0d8330d8edd76556b3b82bf1bca6
Matt <mtthw8198@gmail.com>
2021-12-19 02:19:15 -0800

    fixed inventory slots mapping to each other

--- src/main/java/com/github/thebrochacho/putin/inventory/ContainerPutin.java
+++ src/main/java/com/github/thebrochacho/putin/inventory/ContainerPutin.java
@@ -6,6 +6,9 @@ import net.minecraft.inventory.ContainerPlayer;
 import net.minecraft.inventory.Slot;
 
 public class ContainerPutin extends ContainerPlayer {
+    //Offset so that itemslots don't get mapped to each other
+    private static final int INVENTORY_OFFSET = 27;
+
     public ContainerPutin(InventoryPlayer p_i1819_1_, boolean p_i1819_2_, EntityPlayer p_i1819_3_) {
         super(p_i1819_1_, p_i1819_2_, p_i1819_3_);
 
@@ -13,13 +16,13 @@ public class ContainerPutin extends ContainerPlayer {
         {
             for (int j = 9; j < 18; ++j)
             {
-                this.addSlotToContainer(new Slot(p_i1819_1_, j + (i + 1) * 9, 8 + j * 18, 84 + i * 18));
+                this.addSlotToContainer(new Slot(p_i1819_1_, j + (i + 1) * 9 + INVENTORY_OFFSET, 8 + j * 18, 84 + i * 18));
             }
         }
 
         for (int i = 9; i < 18; ++i)
         {
-            this.addSlotToContainer(new Slot(p_i1819_1_, i, 8 + i * 18, 142));
+            this.addSlotToContainer(new Slot(p_i1819_1_, i + INVENTORY_OFFSET, 8 + i * 18, 142));
         }
     }
 }
