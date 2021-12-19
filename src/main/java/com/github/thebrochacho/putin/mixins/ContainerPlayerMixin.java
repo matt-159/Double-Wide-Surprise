e242b266cec6032e297ee1685d676dd4bd47874c
Matt <mtthw8198@gmail.com>
2021-12-19 02:18:38 -0800

    unified both x-offsets into a single one

--- src/main/java/com/github/thebrochacho/putin/mixins/ContainerPlayerMixin.java
+++ src/main/java/com/github/thebrochacho/putin/mixins/ContainerPlayerMixin.java
@@ -7,15 +7,14 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(ContainerPlayer.class)
 public abstract class ContainerPlayerMixin {
-    private static final int CRAFT_MATRIX_X_OFFSET = 250;
-    private static final int CRAFT_RESULT_X_OFFSET = 306;
+    private static final int CRAFTING_SLOT_X_OFFSET = 162;
 
     @ModifyConstant(method="<init>",
                     constant = @Constant(intValue = 88,
                                         ordinal = 0),
                     require = 1)
     private int shiftCraftingMatrix(int value) {
-        return CRAFT_MATRIX_X_OFFSET;
+        return value + CRAFTING_SLOT_X_OFFSET;
     }
 
     @ModifyConstant(method="<init>",
@@ -23,6 +22,6 @@ public abstract class ContainerPlayerMixin {
                                         ordinal = 0),
                     require = 1)
     private int shiftResultMatrix(int value) {
-        return CRAFT_RESULT_X_OFFSET;
+        return value + CRAFTING_SLOT_X_OFFSET;
     }
 }
