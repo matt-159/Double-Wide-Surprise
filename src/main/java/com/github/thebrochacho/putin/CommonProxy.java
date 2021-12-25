041f148675b836024a72c6c7ae70ee4609025419
Matt <mtthw8198@gmail.com>
2021-12-25 08:16:43 -0800

    added soft dependencies

--- src/main/java/com/github/thebrochacho/putin/CommonProxy.java
+++ src/main/java/com/github/thebrochacho/putin/CommonProxy.java
@@ -1,6 +1,7 @@
 package com.github.thebrochacho.putin;
 
 import com.github.thebrochacho.putin.gui.Handler;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.common.network.NetworkRegistry;
 
@@ -18,6 +19,11 @@ public class CommonProxy {
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes."
     public void init(FMLInitializationEvent event) {
         NetworkRegistry.INSTANCE.registerGuiHandler(Putin.INSTANCE, new Handler());
+
+        Config.isBaublesLoaded = Loader.isModLoaded("Baubles");
+        Config.isTravellersGearLoaded = Loader.isModLoaded("TravellersGear");
+        Config.isTinkersLoaded = Loader.isModLoaded("TConstruct");
+        Config.isGalacticraftLoaded = Loader.isModLoaded("Galacticraft");
     }
 
     // postInit "Handle interaction with other mods, complete your setup based on this."
