164376766cf7414c19de6668a284c5a79a79e271
matt-159 <16054364+matt-159@users.noreply.github.com>
2021-12-25 18:53:48 -0800

    directly set MODID

--- src/main/java/com/github/matt159/putin/Tags.java
+++ src/main/java/com/github/matt159/putin/Tags.java
@@ -5,7 +5,7 @@ package com.github.matt159.putin;
 public class Tags {
 
     // GRADLETOKEN_* will be replaced by your configuration values at build time
-    public static final String MODID = "GRADLETOKEN_MODID";
+    public static final String MODID = "putin";
     public static final String MODNAME = "GRADLETOKEN_MODNAME";
     public static final String VERSION = "GRADLETOKEN_VERSION";
     public static final String GROUPNAME = "com.github.matt159.putin";
