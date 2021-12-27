50f66cca391e31362c1ed9e1039a1f0930fc3f3f
Matt <mtthw8198@gmail.com>
2021-12-26 20:14:38 -0800

    added Tinkers Slots to Putin

--- src/main/java/com/github/thebrochacho/putin/inventory/ContainerPutin.java
+++ src/main/java/com/github/thebrochacho/putin/inventory/ContainerPutin.java
@@ -8,19 +8,27 @@ import com.github.thebrochacho.putin.Config;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ContainerPlayer;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import tconstruct.armor.inventory.SlotAccessory;
+import tconstruct.armor.player.ArmorExtended;
+import tconstruct.armor.player.TPlayerStats;
+import travellersgear.api.TravellersGearAPI;
+import travellersgear.common.inventory.InventoryTG;
+import travellersgear.common.inventory.SlotRestricted;
+import travellersgear.common.inventory.SlotRestricted.SlotType;
 
 public class ContainerPutin extends ContainerPlayer {
     //Offset so that itemslots don't get mapped to each other
     private static final int INVENTORY_OFFSET = 27;
     public static int BAUBLES_SLOT_START = -1;
     public static int TINKERS_SLOT_START = -1;
-    public static int GC_SLOT_START = -1;
     public static int TG_SLOT_START = -1;
+    public static int GC_SLOT_START = -1;
+
 
     private InventoryBaubles baubles;
-    private IInventory tinkers;
+    private ArmorExtended tinkers;
+    private InventoryTG travellers;
 
     public ContainerPutin(InventoryPlayer p_i1819_1_, boolean p_i1819_2_, EntityPlayer p_i1819_3_) {
         super(p_i1819_1_, p_i1819_2_, p_i1819_3_);
@@ -38,25 +46,45 @@ public class ContainerPutin extends ContainerPlayer {
             this.addSlotToContainer(new Slot(p_i1819_1_, i + INVENTORY_OFFSET, 8 + i * 18, 142));
         }
 
+        int xOffset = 80;
+
         if (Config.isBaublesLoaded) {
             if (BAUBLES_SLOT_START == -1) {
                 BAUBLES_SLOT_START = this.inventorySlots.size();
             }
 
-            baubles = new InventoryBaubles(p_i1819_3_);
+            baubles = PlayerHandler.getPlayerBaubles(p_i1819_3_);
             baubles.setEventHandler(this);
             if (!p_i1819_3_.worldObj.isRemote) {
                 baubles.stackList = PlayerHandler.getPlayerBaubles(p_i1819_3_).stackList;
             }
 
-            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.AMULET,0,80,8 + 0 * 18));
-            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.RING,1,80,8 + 1 * 18));
-            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.RING,2,80,8 + 2 * 18));
-            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.BELT,3,80,8 + 3 * 18));
+            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.AMULET,0, xOffset,8 + 0 * 18));
+            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.RING,1, xOffset,8 + 1 * 18));
+            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.RING,2, xOffset,8 + 2 * 18));
+            this.addSlotToContainer(new SlotBauble(baubles, BaubleType.BELT,3, xOffset,8 + 3 * 18));
+
+            //size of one inventory slot;
+            xOffset += 18;
         }
 
         if (Config.isTinkersLoaded) {
+            if (TINKERS_SLOT_START == -1) {
+                TINKERS_SLOT_START = this.inventorySlots.size();
+            }
 
+            tinkers = TPlayerStats.get(p_i1819_3_).armor;
+
+            this.addSlotToContainer(new SlotAccessory(tinkers, 0, xOffset, 8 + 0 * 18, 1));
+            this.addSlotToContainer(new SlotAccessory(tinkers, 1, xOffset, 8 + 1 * 18, 1));
+            this.addSlotToContainer(new SlotAccessory(tinkers, 2, xOffset, 8 + 2 * 18, 1));
+            this.addSlotToContainer(new SlotAccessory(tinkers, 3, xOffset, 8 + 3 * 18, 1));
+            xOffset += 18;
+
+            this.addSlotToContainer(new SlotAccessory(tinkers, 4, xOffset, 8 + 0 * 18, 10));
+            this.addSlotToContainer(new SlotAccessory(tinkers, 5, xOffset, 8 + 1 * 18, 10));
+            this.addSlotToContainer(new SlotAccessory(tinkers, 6, xOffset, 8 + 2 * 18, 10));
+            xOffset += 18;
         }
 
         if (Config.isTravellersGearLoaded) {
