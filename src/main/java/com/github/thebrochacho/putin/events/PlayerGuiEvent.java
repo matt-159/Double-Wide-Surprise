2bd14636902d539ada706fc9cbeeac27e3e41851
Matt <mtthw8198@gmail.com>
2021-12-28 08:28:06 -0800

    removed GearButton from inventory using an event subscription instead

--- src/main/java/com/github/thebrochacho/putin/events/PlayerGuiEvent.java
+++ src/main/java/com/github/thebrochacho/putin/events/PlayerGuiEvent.java
@@ -2,13 +2,16 @@ package com.github.thebrochacho.putin.events;
 
 import com.github.thebrochacho.putin.Putin;
 import com.github.thebrochacho.putin.gui.PutinGui;
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.network.internal.FMLNetworkHandler;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 
 public class PlayerGuiEvent {
 
@@ -19,6 +22,22 @@ public class PlayerGuiEvent {
             event.setCanceled(true);
 
             FMLNetworkHandler.openGui(Minecraft.getMinecraft().thePlayer, Putin.INSTANCE, 1, Minecraft.getMinecraft().theWorld, 0, 0, 0);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    @SideOnly(Side.CLIENT)
+    public void onGuiPostInit(GuiScreenEvent.InitGuiEvent.Post event) {
+        if (event.gui instanceof GuiInventory) {
+            try {
+                final Class<?> TG_GuiButtonGear = Class.forName("travellersgear.client.gui.GuiButtonGear");
+
+                for (Object button : event.buttonList) {
+                    if (TG_GuiButtonGear.isInstance(button)) {
+                        event.buttonList.remove(button);
+                    }
+                }
+            } catch (Exception ignored) { }
         }
     }
 }
