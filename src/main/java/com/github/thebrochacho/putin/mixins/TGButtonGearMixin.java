5e3910002e42f771f3256d3900c4b71576b8a01b
Matt <mtthw8198@gmail.com>
2021-12-28 09:32:21 -0800

    prevent ButtonGear from rendering at all

---
+++ src/main/java/com/github/thebrochacho/putin/mixins/TGButtonGearMixin.java
@@ -0,0 +1,24 @@
+package com.github.thebrochacho.putin.mixins;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import travellersgear.client.gui.GuiButtonGear;
+
+@Mixin(GuiButtonGear.class)
+public abstract class TGButtonGearMixin {
+    @Inject(method = "drawButton",
+            at = @At("HEAD"),
+            remap = false,
+            require = 1)
+    protected void dontDrawButton(Minecraft mc, int x, int y, CallbackInfo ci) {
+        try {
+            ((GuiButton) (Object) this).visible = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
