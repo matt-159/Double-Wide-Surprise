fd388c68c4b3ccfa6b21056a702eff88545de251
Matt <mtthw8198@gmail.com>
2022-01-01 02:01:09 -0800

    Added an interface to certain GC items to use as a filter

---
+++ src/main/java/com/github/thebrochacho/putin/mixins/GalacticraftEquipableMixin.java
@@ -0,0 +1,19 @@
+package com.github.thebrochacho.putin.mixins;
+
+import com.github.thebrochacho.putin.util.IGalacticWearable;
+import micdoodle8.mods.galacticraft.core.items.*;
+import micdoodle8.mods.galacticraft.planets.asteroids.items.ItemThermalPadding;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(value = {ItemOxygenGear.class,
+                ItemOxygenMask.class,
+                ItemOxygenTank.class,
+                ItemParaChute.class,
+                ItemThermalPadding.class,
+                ItemBasic.class})
+@Implements(@Interface(iface = IGalacticWearable.class, prefix = "pu$"))
+public abstract class GalacticraftEquipableMixin{
+    //Pls work
+}
---
+++ src/main/java/com/github/thebrochacho/putin/util/IGalacticWearable.java
@@ -0,0 +1,4 @@
+package com.github.thebrochacho.putin.util;
+
+public interface IGalacticWearable { }
+//just here to make my life a little easier
