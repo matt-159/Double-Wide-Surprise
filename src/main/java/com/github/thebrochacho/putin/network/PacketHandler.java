514f1088c5d3fcae61d4069332979f2f54dc1039
Matt <mtthw8198@gmail.com>
2022-01-14 18:58:53 -0800

    added packet for inventory swaps

---
+++ src/main/java/com/github/thebrochacho/putin/network/PacketHandler.java
@@ -0,0 +1,15 @@
+package com.github.thebrochacho.putin.network;
+
+import com.github.thebrochacho.putin.Tags;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import cpw.mods.fml.relauncher.Side;
+
+public class PacketHandler {
+    public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(Tags.MODID);
+
+    public static void init() {
+        INSTANCE.registerMessage(PutinInventorySwapPacket.class, PutinInventorySwapPacket.class, 0, Side.SERVER);
+        INSTANCE.registerMessage(PutinInventorySwapPacket.class, PutinInventorySwapPacket.class, 0, Side.CLIENT);
+    }
+}
