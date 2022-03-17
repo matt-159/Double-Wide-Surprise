43f05241ec093cf2fc06dcf777bcaa918fe3bd6b
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-03-17 11:58:35 -0700

    created accessible version of static inner classes

---
+++ src/main/java/com/github/matt159/putin/inventory/SlotIngredient.java
@@ -0,0 +1,25 @@
+package com.github.matt159.putin.inventory;
+
+import net.minecraft.inventory.ContainerBrewingStand;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+//Easier to copy paste code than expose a static inner class
+public class SlotIngredient extends Slot {
+    private static final String __OBFID = "CL_00001738";
+    final ContainerBrewingStand this$0;
+
+    public SlotIngredient(ContainerBrewingStand p_i1803_1_, IInventory p_i1803_2_, int p_i1803_3_, int p_i1803_4_, int p_i1803_5_) {
+        super(p_i1803_2_, p_i1803_3_, p_i1803_4_, p_i1803_5_);
+        this.this$0 = p_i1803_1_;
+    }
+
+    public boolean isItemValid(ItemStack p_75214_1_) {
+        return p_75214_1_ != null ? p_75214_1_.getItem().isPotionIngredient(p_75214_1_) : false;
+    }
+
+    public int getSlotStackLimit() {
+        return 64;
+    }
+}
---
+++ src/main/java/com/github/matt159/putin/inventory/SlotPotion.java
@@ -0,0 +1,40 @@
+package com.github.matt159.putin.inventory;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemPotion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.AchievementList;
+
+//Easier to copy paste code than expose a static inner class
+public class SlotPotion extends Slot {
+    private EntityPlayer player;
+    private static final String __OBFID = "CL_00001740";
+
+    public SlotPotion(EntityPlayer p_i1804_1_, IInventory p_i1804_2_, int p_i1804_3_, int p_i1804_4_, int p_i1804_5_) {
+        super(p_i1804_2_, p_i1804_3_, p_i1804_4_, p_i1804_5_);
+        this.player = p_i1804_1_;
+    }
+
+    public boolean isItemValid(ItemStack p_75214_1_) {
+        return canHoldPotion(p_75214_1_);
+    }
+
+    public int getSlotStackLimit() {
+        return 1;
+    }
+
+    public void onPickupFromSlot(EntityPlayer p_82870_1_, ItemStack p_82870_2_) {
+        if (p_82870_2_.getItem() instanceof ItemPotion && p_82870_2_.getItemDamage() > 0) {
+            this.player.addStat(AchievementList.potion, 1);
+        }
+
+        super.onPickupFromSlot(p_82870_1_, p_82870_2_);
+    }
+
+    public static boolean canHoldPotion(ItemStack p_75243_0_) {
+        return p_75243_0_ != null && (p_75243_0_.getItem() instanceof ItemPotion || p_75243_0_.getItem() == Items.glass_bottle);
+    }
+}
