c2631ccf08ffecc20d6aee71b1d6ffaeb4e01d65
Matt <mtthw8198@gmail.com>
2022-03-16 21:24:44 -0700

    putin-ified crafting table

---
+++ src/main/java/com/github/thebrochacho/putin/mixins/client/minecraft/GuiCraftingMixin.java
@@ -0,0 +1,50 @@
+package com.github.thebrochacho.putin.mixins.client.minecraft;
+
+import com.github.thebrochacho.putin.Tags;
+import com.github.thebrochacho.putin.interfaces.IMinecraftGuiMixin;
+import com.github.thebrochacho.putin.util.PutinUtil;
+import net.minecraft.client.gui.inventory.GuiCrafting;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GuiCrafting.class)
+public class GuiCraftingMixin {
+
+    private static final ResourceLocation location = new ResourceLocation(Tags.MODID, "textures/minecraft/crafting_table.png");
+    private static final int X_SIZE = 338;
+    private static final int Y_SIZE = 166;
+
+    @Inject(method = "<init>",
+            at = @At(   value = "RETURN",
+                        remap = false),
+            require = 1)
+    private void updateGuiSize(InventoryPlayer p_i1084_1_, World p_i1084_2_, int p_i1084_3_, int p_i1084_4_, int p_i1084_5_, CallbackInfo ci) {
+        ((GuiCrafting) (Object) (this)).xSize = X_SIZE;
+        ((GuiCrafting) (Object) (this)).ySize = Y_SIZE;
+    }
+
+    @Redirect(  method = "drawGuiContainerBackgroundLayer",
+                at = @At(   value = "INVOKE",
+                            target = "Lnet/minecraft/client/renderer/texture/TextureManager;bindTexture(Lnet/minecraft/util/ResourceLocation;)V",
+                            remap = false),
+                require = 1)
+    private void rerouteBindTexture(TextureManager instance, ResourceLocation p_bindTexture_1_) {
+        instance.bindTexture(location);
+    }
+
+    @Redirect(  method = "drawGuiContainerBackgroundLayer",
+                at = @At(   value = "INVOKE",
+                            target = "Lnet/minecraft/client/gui/inventory/GuiCrafting;drawTexturedModalRect(IIIIII)V"),
+                require = 1)
+    private void rerouteDrawCall(GuiCrafting instance, int x, int y, int u, int v, int w, int h) {
+        float zLevel = ((IMinecraftGuiMixin) (Object) (this)).getZLevel();
+        PutinUtil.drawTexturedModalRect(x, y, u, v, w, h, zLevel);
+    }
+}
