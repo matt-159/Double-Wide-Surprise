de17925c19c606099319882a51f310279f136edb
Matt <mtthw8198@gmail.com>
2022-03-18 14:40:22 -0700

    created copies of anonymous classes

---
+++ src/main/java/com/github/thebrochacho/putin/inventory/SlotSaddle.java
@@ -0,0 +1,18 @@
+package com.github.thebrochacho.putin.inventory;
+
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+
+public class SlotSaddle extends Slot {
+
+    public SlotSaddle(IInventory horseInventory, int p_i1824_2_, int p_i1824_3_, int p_i1824_4_) {
+        super(horseInventory, p_i1824_2_, p_i1824_3_, p_i1824_4_);
+    }
+
+    @Override
+    public boolean isItemValid(ItemStack itemStack) {
+        return super.isItemValid(itemStack) && itemStack.getItem() == Items.saddle && !this.getHasStack();
+    }
+}
