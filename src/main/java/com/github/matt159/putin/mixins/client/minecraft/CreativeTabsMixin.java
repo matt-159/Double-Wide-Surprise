7bd049c6178963b5e7dcb95dca4a22da7b79061b
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-03-22 08:12:09 -0700

    checkpoint

---
+++ src/main/java/com/github/matt159/putin/mixins/client/minecraft/CreativeTabsMixin.java
@@ -0,0 +1,44 @@
+package com.github.matt159.putin.mixins.client.minecraft;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@SideOnly(Side.CLIENT)
+@Mixin(CreativeTabs.class)
+public class CreativeTabsMixin {
+
+    @Shadow @Final private int tabIndex;
+
+    @SideOnly(Side.CLIENT)
+    @Inject(method = "getTabColumn",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void getTabColumn(CallbackInfoReturnable<Integer> cir) {
+        int ret = this.tabIndex > 21 ? (this.tabIndex - 22) % 10 : this.tabIndex % 11;
+        cir.setReturnValue(ret);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Inject(method = "isTabInFirstRow",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void isTabInFirstRow(CallbackInfoReturnable<Boolean> cir) {
+        boolean ret;
+
+        if (this.tabIndex > 21) {
+            ret = (this.tabIndex - 22) % 20 < 10;
+        } else {
+            ret = this.tabIndex < 11;
+        }
+
+        cir.setReturnValue(ret);
+    }
+
+}
