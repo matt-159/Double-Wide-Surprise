e708646cf1a0a5bbb0ff161ce4139941a498e6e3
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-03-30 19:26:23 -0700

    Exposed GuiBeacon private non-static classes

---
+++ src/main/java/com/github/matt159/putin/gui/minecraft/GuiBeaconCancelButton.java
@@ -0,0 +1,10 @@
+package com.github.matt159.putin.gui.minecraft;
+
+import net.minecraft.client.gui.inventory.GuiBeacon;
+
+public class GuiBeaconCancelButton extends GuiBeacon.CancelButton {
+
+    public GuiBeaconCancelButton(GuiBeacon guiBeacon, int id, int xPosition, int yPosition) {
+        guiBeacon.super(id, xPosition, yPosition);
+    }
+}
---
+++ src/main/java/com/github/matt159/putin/gui/minecraft/GuiBeaconPowerButton.java
@@ -0,0 +1,10 @@
+package com.github.matt159.putin.gui.minecraft;
+
+import net.minecraft.client.gui.inventory.GuiBeacon;
+
+public class GuiBeaconPowerButton extends GuiBeacon.PowerButton {
+
+    public GuiBeaconPowerButton(GuiBeacon guiBeacon, int id, int xPosition, int yPosition, int p_i1076_5_, int p_i1076_6_) {
+        guiBeacon.super(id, xPosition, yPosition, p_i1076_5_, p_i1076_6_);
+    }
+}
