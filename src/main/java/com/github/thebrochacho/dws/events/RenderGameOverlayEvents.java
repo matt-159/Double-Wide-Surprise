1090fa09578de26a2dc0abb739c79e76dd594f13
matt-159 <mtthw8198@gmail.com>
2022-05-23 12:06:22 -0700

    hotbar render event set to lowest priority

--- src/main/java/com/github/thebrochacho/dws/events/RenderGameOverlayEvents.java
+++ src/main/java/com/github/thebrochacho/dws/events/RenderGameOverlayEvents.java
@@ -1,6 +1,7 @@
 package com.github.thebrochacho.dws.events;
 
 import com.github.thebrochacho.dws.gui.minecraft.DWSHotbar;
+import cpw.mods.fml.common.eventhandler.EventPriority;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -10,7 +11,7 @@ import net.minecraftforge.common.MinecraftForge;
 public class RenderGameOverlayEvents {
 
     @SideOnly(Side.CLIENT)
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public void guiPreAction(RenderGameOverlayEvent.Pre event) {
         if (event.type == RenderGameOverlayEvent.ElementType.HOTBAR) {
             event.setCanceled(true);
