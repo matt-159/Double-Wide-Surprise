b6f6135eb6e7f136ee8454639e1893aa14b5c8d4
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-05-23 13:09:27 -0700

    fixed TG button location when a potion effect is active

--- src/main/java/com/github/matt159/dws/mixins/client/travellersgear/ClientProxyMixin.java
+++ src/main/java/com/github/matt159/dws/mixins/client/travellersgear/ClientProxyMixin.java
@@ -1,8 +1,10 @@
 package com.github.matt159.dws.mixins.client.travellersgear;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import travellersgear.client.ClientProxy;
 
 @Mixin(ClientProxy.class)
@@ -14,5 +16,15 @@ public class ClientProxyMixin {
                     require = 1)
     private int modifyXSize(int constant) {
         return 338;
+    }
+
+    // Lmao. Budget ASM
+    @Redirect(  method = "guiPostInit",
+                at = @At(   value = "INVOKE",
+                            target = "Ltravellersgear/common/util/ModCompatability;isNeiHidden()Z"),
+                remap = false,
+                require = 1)
+    private boolean redirectIsHidden() {
+        return false;
     }
 }
