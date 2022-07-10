0aecc75fafa29fb3b80226b4fb183976cd388dab
matt-159 <mtthw8198@gmail.com>
2022-07-09 20:26:33 -0700

    applied an x-offset for all basic machine slots

--- src/main/java/com/github/thebrochacho/dws/mixins/plugin/Mixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixins/plugin/Mixin.java
@@ -93,6 +93,7 @@ public enum Mixin {
     GT_ContainerMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_ContainerMixin")),
 //    GT_GUIContainerMixin(builder(Side.CLIENT).unit(CompatibilityTier.Regular, "gregtech.GT_GUIContainerMixin")),
 
+    GT_Container_BasicMachineMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_BasicMachineMixin")),
     GT_Container_1by1Mixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_1by1Mixin")),
     ;
 
