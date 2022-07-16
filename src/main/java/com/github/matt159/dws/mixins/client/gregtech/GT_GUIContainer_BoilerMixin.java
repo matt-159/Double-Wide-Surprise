c621bf9f958eed72a914b73856dad96fad3b1f4c
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-07-16 16:48:26 -0700

    adjusted slots and display bars for gt boilers

---
+++ src/main/java/com/github/matt159/dws/mixins/client/gregtech/GT_GUIContainer_BoilerMixin.java
@@ -0,0 +1,38 @@
+package com.github.matt159.dws.mixins.client.gregtech;
+
+import gregtech.api.gui.GT_GUIContainerMetaTile_Machine;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
+import gregtech.common.gui.GT_GUIContainer_Boiler;
+import net.minecraft.entity.player.InventoryPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GT_GUIContainer_Boiler.class)
+public class GT_GUIContainer_BoilerMixin {
+    @ModifyConstant(method = "drawGuiContainerBackgroundLayer",
+                    constant = {
+                        @Constant(intValue = 70),
+                        @Constant(intValue = 83),
+                        @Constant(intValue = 96),
+                        @Constant(intValue = 117)
+                    },
+                    remap = false,
+                    require = 1)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
+    }
+
+    @ModifyConstant(method = "drawGuiContainerBackgroundLayer",
+                    constant = {
+                        @Constant(intValue = 177),
+                        @Constant(intValue = 194),
+                        @Constant(intValue = 204),
+                        @Constant(intValue = 214)
+                    },
+                    remap = false,
+                    require = 1)
+    private int modifyTextureXOffset(int constant) {
+        return constant + 162;
+    }
+}
