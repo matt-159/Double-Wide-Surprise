00ef499037836f9f372aa218bc77c215b782084a
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-07-17 13:57:44 -0700

    Finished compatibility for GT Fluid Output Hatches

---
+++ src/main/java/com/github/matt159/dws/mixins/client/gregtech/GT_GUIContainer_OutputHatchMixin.java
@@ -0,0 +1,20 @@
+package com.github.matt159.dws.mixins.client.gregtech;
+
+import gregtech.common.gui.GT_GUIContainer_OutputHatch;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GT_GUIContainer_OutputHatch.class)
+public abstract class GT_GUIContainer_OutputHatchMixin {
+    @ModifyConstant(method = "drawGuiContainerForegroundLayer",
+                    constant = {
+                        @Constant(intValue = 10),
+                        @Constant(intValue = 101)
+                    },
+                    remap = false,
+                    require = 1)
+    private int modifyDrawStringXOffset(int constant) {
+        return constant + 81;
+    }
+}
