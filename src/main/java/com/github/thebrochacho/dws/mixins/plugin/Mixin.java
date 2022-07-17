25ce142942c7c40fa5e0e990faf47ffeafa02185
Matt <16054364+matt-159@users.noreply.github.com>
2022-07-17 08:57:00 -0700

    Added compatibility for GT Item Filter

--- src/main/java/com/github/thebrochacho/dws/mixins/plugin/Mixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixins/plugin/Mixin.java
@@ -78,6 +78,7 @@ public enum Mixin {
     GT_Container_BasicMachineMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_BasicMachineMixin")),
     GT_Container_BasicTankMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_BasicTankMixin")),
     GT_Container_BoilerMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_BoilerMixin")),
+    GT_Container_FilterMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_FilterMixin")),
     GT_Container_MaintenanceHatchMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_MaintenanceHatchMixin")),
     GT_ContainerMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_ContainerMixin")),
     GT_Container_PrimitiveBlastFurnaceMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_PrimitiveBlastFurnaceMixin")),
