473d06ad71ecdca7e5729ffd263592431a4e8ac2
Matt <16054364+matt-159@users.noreply.github.com>
2022-07-21 10:01:43 -0700

    adjusted slot location in GT Multis

--- src/main/java/com/github/thebrochacho/dws/mixins/plugin/Mixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixins/plugin/Mixin.java
@@ -84,6 +84,7 @@ public enum Mixin {
     GT_Container_ItemDistributorMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_ItemDistributorMixin")),
     GT_Container_MaintenanceHatchMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_MaintenanceHatchMixin")),
     GT_Container_MicrowaveEnergyTransmitterMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_MicrowaveEnergyTransmitterMixin")),
+    GT_Container_MultiMachineMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_MultiMachineMixin")),
     GT_Container_OutputHatchMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_OutputHatchMixin")),
     GT_Container_PrimitiveBlastFurnaceMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_PrimitiveBlastFurnaceMixin")),
     GT_Container_QuantumChestMixin(builder(Side.COMMON).unit(CompatibilityTier.Regular, "gregtech.GT_Container_QuantumChestMixin")),
