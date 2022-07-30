3faa1b2616d36df5de77ccb3bed1610457c338b3
Matt <16054364+matt-159@users.noreply.github.com>
2022-07-30 05:51:52 -0700

    ran 'Optimize Imports' on project

--- src/main/java/com/github/thebrochacho/dws/mixins/client/minecraft/GuiFurnaceMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixins/client/minecraft/GuiFurnaceMixin.java
@@ -1,18 +1,12 @@
 package com.github.thebrochacho.dws.mixins.client.minecraft;
 
 import com.github.thebrochacho.dws.interfaces.IDWSGui;
-import com.github.thebrochacho.dws.util.DWSUtil;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.gui.inventory.GuiFurnace;
 import net.minecraft.inventory.Container;
-import net.minecraft.tileentity.TileEntityFurnace;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiFurnace.class)
 public abstract class GuiFurnaceMixin extends GuiContainer implements IDWSGui {
