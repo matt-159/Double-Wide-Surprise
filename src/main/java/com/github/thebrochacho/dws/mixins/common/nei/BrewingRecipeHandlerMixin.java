34404d2c86d81c02408e511c21a7ee42a56ddbaf
Matt <16054364+matt-159@users.noreply.github.com>
2022-07-30 10:39:25 -0700

    adjusted offset of NEI "recipe" tooltip/button for crafting table and brewing stand

---
+++ src/main/java/com/github/thebrochacho/dws/mixins/common/nei/BrewingRecipeHandlerMixin.java
@@ -0,0 +1,21 @@
+package com.github.thebrochacho.dws.mixins.common.nei;
+
+import codechicken.nei.recipe.BrewingRecipeHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(BrewingRecipeHandler.class)
+public abstract class BrewingRecipeHandlerMixin {
+    @ModifyConstant(method = "loadTransferRects",
+                    constant = {
+                        @Constant(intValue = 58),
+                        @Constant(intValue = 68),
+                        @Constant(intValue = 92)
+                    },
+                    remap = false,
+                    require = 1)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
+    }
+}
