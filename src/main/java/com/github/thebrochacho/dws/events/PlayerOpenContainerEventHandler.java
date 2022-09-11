75676f51fc12e7524f83fb9d6ed06e4b3a39f67b
basdxz <tudurap.com@gmail.com>
2022-09-11 05:15:36 +0100

    FIX: Launching in dev without other mods present

--- src/main/java/com/github/thebrochacho/dws/events/PlayerOpenContainerEventHandler.java
+++ src/main/java/com/github/thebrochacho/dws/events/PlayerOpenContainerEventHandler.java
@@ -7,12 +7,16 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.inventory.Container;
 import net.minecraftforge.event.entity.player.PlayerOpenContainerEvent;
 
+import static com.github.thebrochacho.dws.util.ModCompat.isBaublesPresent;
+
 public class PlayerOpenContainerEventHandler {
     /*  The InventoryBaubles object inside ContainerDWS is likely to get constructed prior to the player's baubles being
      *  loaded from file. This forces synchronization of the player's baubles
      */
     @SubscribeEvent
     public void onPlayerOpenContainer(PlayerOpenContainerEvent event) {
+        if (!isBaublesPresent())
+            return;
         Container container = event.entityPlayer.inventoryContainer;
         InventoryBaubles baubles = PlayerHandler.getPlayerBaubles(event.entityPlayer);
 
