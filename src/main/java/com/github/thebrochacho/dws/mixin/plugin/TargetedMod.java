abae3d1ec0c1cfa930fb9b535a6ccdd891e67210
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-13 10:13:49 -0700

    set mods to load in dev

--- src/main/java/com/github/thebrochacho/dws/mixin/plugin/TargetedMod.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/plugin/TargetedMod.java
@@ -13,16 +13,17 @@ import static com.falsepattern.lib.mixin.ITargetedMod.PredicateHelpers.startsWit
 @Getter
 @RequiredArgsConstructor
 public enum TargetedMod implements ITargetedMod {
-    GALACTICRAFT("Galacticraft", false, startsWith("galacticraft")),
-    TRAVELLERSGEAR("TravellersGear", false, startsWith("traveller")),
-    IRONCHEST("IronChest", false, startsWith("ironchest")),
-    BAUBLES("Baubles", false, startsWith("baubles")),
-    TINKERSCONSTRUCT("TConstruct", false, startsWith("construct")),
-    CODECHICKENLIB("CodeChickenLib", false, startsWith("codechicken")),
-    NOTENOUGHITEMS("NotEnoughItems", false, startsWith("notenoughitems")),
-    GREGTECH("GregTech", false, startsWith("gregtech").or(startsWith("gt5u"))),
-    BARTWORKS("Bartworks", false, startsWith("bartworks")),
-    TECTECH("TecTech", false, startsWith("tectech").or(startsWith("tec-tech")));
+    GALACTICRAFT      ("Galacticraft"    , false, startsWith("galacticraft")),
+    TRAVELLERSGEAR    ("TravellersGear"  , true, startsWith("traveller")),
+    IRONCHEST         ("IronChest"       , true, startsWith("ironchest")),
+    BAUBLES           ("Baubles"         , true, startsWith("baubles")),
+    TINKERSCONSTRUCT  ("TConstruct"      , true, startsWith("construct")),
+    CODECHICKENLIB    ("CodeChickenLib"  , true, startsWith("codechicken")),
+    NOTENOUGHITEMS    ("NotEnoughItems"  , true, startsWith("notenoughitems")),
+    GREGTECH          ("GregTech"        , true, startsWith("gregtech").or(startsWith("gt5u"))),
+    BARTWORKS         ("Bartworks"       , true, startsWith("bartworks")),
+    TECTECH           ("TecTech"         , true, startsWith("tectech").or(startsWith("tec-tech")));
+
 
     private final String modName;
     private final boolean loadInDevelopment;
