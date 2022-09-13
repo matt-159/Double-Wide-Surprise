a0c3621b8accf2bc6782a3d2627abe4a04805640
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-13 03:33:06 -0700

    migrated from an @Inject mixin to @ModifyConstant mixins

--- src/main/java/com/github/thebrochacho/dws/mixins/common/minecraft/inventory/ContainerChestMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixins/common/minecraft/inventory/ContainerChestMixin.java
@@ -5,33 +5,32 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerChest.class)
 public abstract class ContainerChestMixin extends Container {
 
-    @Shadow private int numRows;
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(   intValue = 8,
+                                            ordinal = 0),
+                    require = 1)
+    private int modifyChestSlotsXOffset(int constant) {
+        return constant + 81;
+    }
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(IInventory inventoryPlayer, IInventory inventoryChest, CallbackInfo ci) {
-        this.inventorySlots.clear();
-
-        int var3 = (numRows - 4) * 18;
-
-        int row;
-        int col;
-        for(row = 0; row < this.numRows; ++row) {
-            for(col = 0; col < 9; ++col) {
-                this.addSlotToContainer(new Slot(inventoryChest, col + row * 9, 89 + col * 18, 18 + row * 18));
-            }
-        }
-
-        DWSUtil.addDWSSlotsToContainer(this, inventoryPlayer, 8, 103 + var3, 161 + var3);
+    @ModifyConstant(method = "<init>",
+                    slice = @Slice( from =  @At(value = "INVOKE",
+                                                target = "Lnet/minecraft/inventory/ContainerChest;addSlotToContainer(Lnet/minecraft/inventory/Slot;)Lnet/minecraft/inventory/Slot;",
+                                                ordinal = 0,
+                                                shift = At.Shift.AFTER),
+                                    to =    @At("RETURN")),
+                    constant = @Constant(intValue = 9),
+                    require = 3)
+    private int modifyPlayerInventorySlotCount(int constant) {
+        return 18;
     }
 }
