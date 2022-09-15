1de8032f2eec1053c9aae400d6129e2fb2ecfb47
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-15 08:23:19 -0700

    converted @Inject into a @ModifyConstant

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/client/minecraft/gui/GuiScreenHorseInventoryMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/client/minecraft/gui/GuiScreenHorseInventoryMixin.java
@@ -10,44 +10,24 @@ import net.minecraft.inventory.Container;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiScreenHorseInventory.class)
 public abstract class GuiScreenHorseInventoryMixin extends GuiContainer implements IDWSGui {
 
-    @Shadow private EntityHorse field_147034_x;
-    @Shadow private float field_147033_y;
-    @Shadow private float field_147032_z;
-
     public GuiScreenHorseInventoryMixin(Container container) {
         super(container);
     }
 
-    @Inject(method = "drawGuiContainerBackgroundLayer",
-            at = @At(   value = "INVOKE",
-                        target = "Lnet/minecraft/client/gui/inventory/GuiScreenHorseInventory;drawTexturedModalRect(IIIIII)V",
-                        ordinal = 0,
-                        shift = At.Shift.BEFORE),
-            cancellable = true,
-            require = 1)
-    private void rerouteDrawCall(float f1, int i1, int i2, CallbackInfo ci) {
-        GuiScreenHorseInventory gshi = (GuiScreenHorseInventory) (Object) (this);
-
-        int x = (gshi.width - gshi.xSize) / 2;
-        int y = (gshi.height - gshi.ySize) / 2;
-        DWSUtil.drawTexturedModalRect(x, y, 0, 0, gshi.xSize, gshi.ySize, zLevel);
-
-        if (this.field_147034_x.isChested()) {
-            DWSUtil.drawTexturedModalRect(x + 160, y + 17, 0, gshi.ySize, 90, 54, zLevel);
-        }
-
-        if (this.field_147034_x.func_110259_cr()) {
-            DWSUtil.drawTexturedModalRect(x + 88, y + 35, 0, gshi.ySize + 54, 18, 18, zLevel);
-        }
-
-        GuiInventory.func_147046_a(x + 132, y + 60, 17, (float)(x + 132) - this.field_147033_y, (float)(y + 75 - 50) - this.field_147032_z, this.field_147034_x);
-
-        ci.cancel();
+    @ModifyConstant(method = "drawGuiContainerBackgroundLayer",
+                    constant = {    @Constant(intValue = 79),
+                                    @Constant(intValue = 7),
+                                    @Constant(intValue = 51)    },
+                    require = 3)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
     }
 }
