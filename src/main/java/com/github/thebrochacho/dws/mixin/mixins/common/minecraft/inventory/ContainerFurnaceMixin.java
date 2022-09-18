d4cab523452f4587ff3cee8e0e529aa1da2c84df
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-18 07:04:02 -0700

    replaced @Inject mixin with @ModifyConstant's

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerFurnaceMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerFurnaceMixin.java
@@ -9,22 +9,26 @@ import net.minecraft.inventory.SlotFurnace;
 import net.minecraft.tileentity.TileEntityFurnace;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerFurnace.class)
 public abstract class ContainerFurnaceMixin extends Container {
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(InventoryPlayer inventoryPlayer, TileEntityFurnace tileEntityFurnace, CallbackInfo ci) {
-       this.inventorySlots.clear();
+    @ModifyConstant(method = "<init>",
+                    constant = {    @Constant(intValue = 56),
+                                    @Constant(intValue = 116)   },
+                    require = 3)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
+    }
 
-        this.addSlotToContainer(new Slot(tileEntityFurnace, 0, 137, 17));
-        this.addSlotToContainer(new Slot(tileEntityFurnace, 1, 137, 53));
-        this.addSlotToContainer(new SlotFurnace(inventoryPlayer.player, tileEntityFurnace, 2, 197, 35));
-
-        DWSUtil.addDWSSlotsToContainer(this, inventoryPlayer);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
