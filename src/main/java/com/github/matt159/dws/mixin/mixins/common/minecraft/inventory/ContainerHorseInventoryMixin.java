ffea0b283559b2ca96c80cbb8d889884faefecc3
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-09-18 07:02:06 -0700

    replaced @Inject mixin with a combination of @Redirects and @ModifyConstants

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerHorseInventoryMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerHorseInventoryMixin.java
@@ -9,33 +9,44 @@ import net.minecraft.inventory.ContainerHorseInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerHorseInventory.class)
 public abstract class ContainerHorseInventoryMixin extends Container {
+    @Shadow
+    private EntityHorse theHorse;
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(IInventory inventoryPlayer, IInventory inventoryHorse, EntityHorse horse, CallbackInfo ci) {
-        this.inventorySlots.clear();
+    @Redirect(  method = "<init>",
+                at = @At(   value = "INVOKE",
+                            target = "Lnet/minecraft/inventory/ContainerHorseInventory;addSlotToContainer(Lnet/minecraft/inventory/Slot;)Lnet/minecraft/inventory/Slot;",
+                            ordinal = 0),
+                require = 1)
+    private Slot redirectAddSlotSaddle(ContainerHorseInventory instance, Slot slot) {
+        return this.addSlotToContainer(new SlotSaddle(slot.inventory, 0, 8 + 81, 18));
+    }
 
-        this.addSlotToContainer(new SlotSaddle(inventoryHorse, 0, 89, 18));
-        this.addSlotToContainer(new SlotHorseArmor(horse, inventoryHorse, 1, 89, 36));
+    @Redirect(  method = "<init>",
+                at = @At(   value = "INVOKE",
+                            target = "Lnet/minecraft/inventory/ContainerHorseInventory;addSlotToContainer(Lnet/minecraft/inventory/Slot;)Lnet/minecraft/inventory/Slot;",
+                            ordinal = 1),
+                require = 1)
+    private Slot redirectAddSlotHorseArmor(ContainerHorseInventory instance, Slot slot) {
+        return this.addSlotToContainer(new SlotHorseArmor(this.theHorse, slot.inventory, 1, 8 + 81, 36));
+    }
 
-        int var4 = 3;
-        int row, col;
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 80),
+                    require = 1)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
+    }
 
-        if (horse.isChested()) {
-            for(row = 0; row < var4; ++row) {
-                for(col = 0; col < 5; ++col) {
-                    this.addSlotToContainer(new Slot(inventoryHorse, 2 + col + row * 5, 161 + col * 18, 18 + row * 18));
-                }
-            }
-        }
-
-        DWSUtil.addDWSSlotsToContainer(this, inventoryPlayer);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
