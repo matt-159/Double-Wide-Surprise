a0d7b8103453ca699044975d64aa0c12e2554ace
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-17 20:50:59 -0700

    changed from an @Inject mixin to a @ModifyConstant

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerDispenserMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerDispenserMixin.java
@@ -8,26 +8,18 @@ import net.minecraft.inventory.Slot;
 import net.minecraft.tileentity.TileEntityDispenser;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerDispenser.class)
 public abstract class ContainerDispenserMixin extends Container {
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(IInventory inventoryPlayer, TileEntityDispenser tileEntityDispenser, CallbackInfo ci) {
-        this.inventorySlots.clear();
-
-        int row;
-        int col;
-        for(row = 0; row < 3; ++row) {
-            for(col = 0; col < 3; ++col) {
-                this.addSlotToContainer(new Slot(tileEntityDispenser, col + row * 3, 143 + col * 18, 17 + row * 18));
-            }
-        }
-
-        DWSUtil.addDWSSlotsToContainer(this, inventoryPlayer);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
