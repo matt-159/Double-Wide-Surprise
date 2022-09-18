8a3b8c2875c27f457580f68d97919e2e72dddef0
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-17 20:51:19 -0700

    changed from an @Inject mixin to a @ModifyConstant

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerBrewingStandMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerBrewingStandMixin.java
@@ -3,6 +3,7 @@ package com.github.thebrochacho.dws.mixin.mixins.common.minecraft.inventory;
 import com.github.thebrochacho.dws.inventory.slots.minecraft.SlotIngredient;
 import com.github.thebrochacho.dws.inventory.slots.minecraft.SlotPotion;
 import com.github.thebrochacho.dws.util.DWSUtil;
+import cpw.mods.fml.common.Mod;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerBrewingStand;
@@ -13,25 +14,27 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerBrewingStand.class)
 public abstract class ContainerBrewingStandMixin extends Container {
 
-    @Mutable
-    @Shadow @Final private Slot theSlot;
+    @ModifyConstant(method = "<init>",
+                    constant = {    @Constant(intValue = 56),
+                                    @Constant(intValue = 79),
+                                    @Constant(intValue = 102)   },
+                    require = 4)
+    private int modifyBrewingSlotsXOffset(int constant) {
+        return constant + 81;
+    }
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(InventoryPlayer inventoryPlayer, TileEntityBrewingStand tileEntityBrewingStand, CallbackInfo ci) {
-        this.inventorySlots.clear();
-        this.addSlotToContainer(new SlotPotion(inventoryPlayer.player, tileEntityBrewingStand, 0, 137, 46));
-        this.addSlotToContainer(new SlotPotion(inventoryPlayer.player, tileEntityBrewingStand, 1, 160, 53));
-        this.addSlotToContainer(new SlotPotion(inventoryPlayer.player, tileEntityBrewingStand, 2, 183, 46));
-        this.theSlot = this.addSlotToContainer(new SlotIngredient((ContainerBrewingStand) (Object) (this), tileEntityBrewingStand, 3, 160, 17));
-
-        DWSUtil.addDWSSlotsToContainer((ContainerBrewingStand) (Object) (this), inventoryPlayer);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
