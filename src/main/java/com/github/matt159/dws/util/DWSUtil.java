dc60e96801e29c22e565746205d041db74057a10
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-09-17 20:53:52 -0700

    removed unused draw call

--- src/main/java/com/github/matt159/dws/util/DWSUtil.java
+++ src/main/java/com/github/matt159/dws/util/DWSUtil.java
@@ -17,19 +17,6 @@ import java.util.Collections;
 import java.util.List;
 
 public final class DWSUtil {
-    //Need to override because this function in Gui.java only allows for 256x256 textures at max
-    public static void drawTexturedModalRect(int x, int y, int u, int v, int width, int height, float zLevel)
-    {
-        float f = 0.001953125F; // (1 / 512)
-        Tessellator tessellator = Tessellator.instance;
-        tessellator.startDrawingQuads();
-        tessellator.addVertexWithUV((x + 0),    (y + height),   zLevel,(u + 0) * f,    (v + height) * f);
-        tessellator.addVertexWithUV((x + width),(y + height),   zLevel,(u + width) * f,(v + height) * f);
-        tessellator.addVertexWithUV((x + width),(y + 0),        zLevel,(u + width) * f,(v + 0) * f);
-        tessellator.addVertexWithUV((x + 0),    (y + 0),        zLevel,(u + 0) * f,    (v + 0) * f);
-        tessellator.draw();
-    }
-
     //allow for drawing of NxM textures
     public static void drawTexturedRect(@Nonnull ResourceLocation textureResourceLocation,
                                         int x,
