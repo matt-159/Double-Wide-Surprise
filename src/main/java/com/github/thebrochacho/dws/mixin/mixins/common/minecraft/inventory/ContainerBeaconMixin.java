cb94dc1174827ca95c02b23e75eb60475ebad79c
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-17 20:51:35 -0700

    changed from an @Inject mixin to a @ModifyConstant

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerBeaconMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerBeaconMixin.java
@@ -11,24 +11,47 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerBeacon.class)
 public abstract class ContainerBeaconMixin extends Container {
 
-    @Mutable
-    @Shadow @Final private ContainerBeacon.BeaconSlot beaconSlot;
+//    @Mutable
+//    @Shadow @Final private ContainerBeacon.BeaconSlot beaconSlot;
+//
+//    @Inject(method = "<init>",
+//            at = @At(value = "RETURN"),
+//            require = 1)
+//    private void addSlotsToContainer(InventoryPlayer inventoryPlayer, TileEntityBeacon tileEntityBeacon, CallbackInfo ci) {
+//        this.inventorySlots.clear();
+//
+//        this.beaconSlot = new SlotBeacon((ContainerBeacon) (Object) (this), tileEntityBeacon, 0, 190, 110);
+//        this.addSlotToContainer(beaconSlot);
+//
+//        DWSUtil.addDWSSlotsToContainer((ContainerBeacon) (Object) (this), inventoryPlayer, 8, 137, 195);
+//    }
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(InventoryPlayer inventoryPlayer, TileEntityBeacon tileEntityBeacon, CallbackInfo ci) {
-        this.inventorySlots.clear();
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 136),
+                    require = 1)
+    private int modifyBeaconSlotXDisplayPosition(int constant) {
+        return constant + 54;
+    }
 
-        this.beaconSlot = new SlotBeacon((ContainerBeacon) (Object) (this), tileEntityBeacon, 0, 190, 110);
-        this.addSlotToContainer(beaconSlot);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 36),
+                    require = 1)
+    private int modifyXOffset(int constant) {
+        return 8;
+    }
 
-        DWSUtil.addDWSSlotsToContainer((ContainerBeacon) (Object) (this), inventoryPlayer, 8, 137, 195);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
