f09fee956be033668922bc207339669aed56201f
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-09-18 07:02:58 -0700

    replaced @Inject mixin with @ModifyConstants

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerWorkbenchMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerWorkbenchMixin.java
@@ -6,29 +6,26 @@ import net.minecraft.inventory.*;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerWorkbench.class)
 public abstract class ContainerWorkbenchMixin extends Container {
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(InventoryPlayer inventoryPlayer, World world, int x, int y, int z, CallbackInfo ci) {
-        this.inventorySlots.clear();
-        InventoryCrafting craftMatrix = ((ContainerWorkbench) (Object) (this)).craftMatrix;
+    @ModifyConstant(method = "<init>",
+                    constant = {    @Constant(intValue = 30),
+                                    @Constant(intValue = 124)   },
+                    require = 2)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
+    }
 
-        this.addSlotToContainer(new SlotCrafting(inventoryPlayer.player, craftMatrix, ((ContainerWorkbench) (Object) (this)).craftResult, 0, 205, 35));
-
-        int row;
-        int col;
-        for(row = 0; row < 3; ++row) {
-            for(col = 0; col < 3; ++col) {
-                this.addSlotToContainer(new Slot(craftMatrix, col + row * 3, 111 + col * 18, 17 + row * 18));
-            }
-        }
-
-        DWSUtil.addDWSSlotsToContainer(this, inventoryPlayer);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
