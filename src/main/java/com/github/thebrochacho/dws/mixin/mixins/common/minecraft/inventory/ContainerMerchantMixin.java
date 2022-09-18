8aa4cc31994ffbf43c09f0cf9fa0af7ebf91d7f2
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-18 07:02:32 -0700

    replaced @Inject mixin with @ModifyConstants

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerMerchantMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/minecraft/inventory/ContainerMerchantMixin.java
@@ -1,31 +1,32 @@
 package com.github.thebrochacho.dws.mixin.mixins.common.minecraft.inventory;
 
 import com.github.thebrochacho.dws.util.DWSUtil;
+import cpw.mods.fml.common.Mod;
 import net.minecraft.entity.IMerchant;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.*;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ContainerMerchant.class)
 public abstract class ContainerMerchantMixin extends Container {
 
-    @Shadow private InventoryMerchant merchantInventory;
+    @ModifyConstant(method = "<init>",
+                    constant = {    @Constant(intValue = 36),
+                                    @Constant(intValue = 62),
+                                    @Constant(intValue = 120)   },
+                    require = 3)
+    private int modifyXOffset(int constant) {
+        return constant + 81;
+    }
 
-    @Inject(method = "<init>",
-            at = @At(value = "RETURN"),
-            require = 1)
-    private void addSlotsToContainer(InventoryPlayer inventoryPlayer, IMerchant merchant, World world, CallbackInfo ci) {
-        this.inventorySlots.clear();
-
-        this.addSlotToContainer(new Slot(this.merchantInventory,0, 117, 53));
-        this.addSlotToContainer(new Slot(this.merchantInventory, 1, 143, 53));
-        this.addSlotToContainer(new SlotMerchantResult(inventoryPlayer.player, merchant, this.merchantInventory, 2, 201, 53));
-
-        DWSUtil.addDWSSlotsToContainer(this, inventoryPlayer);
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 9),
+                    require = 4)
+    private int modifyPlayerInventorySize(int constant) {
+        return 18;
     }
 }
