887098cc1c24e917e23d985e9ef07c508110f893
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-09-19 09:20:03 -0700

    fixed issue with creative pick-block where it would give a ghost item

--- src/main/java/com/github/matt159/dws/mixin/mixins/client/minecraft/MinecraftMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/minecraft/MinecraftMixin.java
@@ -4,14 +4,25 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.List;
 
 @Mixin(Minecraft.class)
 public abstract class MinecraftMixin {
 
-    @Shadow(aliases = "field_71439_g")
-    public EntityClientPlayerMP thePlayer;
+    // inventorySlots.size() can't be trusted to provide a constant number of slots
+    // since accessory slots are added to the ContainerPlayer object
+    @Redirect(  method = "func_147112_ai",
+                at = @At(   value = "INVOKE",
+                            target = "Ljava/util/List;size()I"),
+                require = 1)
+    private int redirectGetMainInventorySize(List instance) {
+        return 81;
+    }
 
     @ModifyConstant(method="func_147112_ai",
                     constant = @Constant(intValue = 9),
@@ -19,14 +30,4 @@ public abstract class MinecraftMixin {
     private int modifyHotbarOffset(int constant) {
         return 18;
     }
-
-//    @ModifyVariable(method = "func_147112_ai",
-//                    at = @At(   value = "LOAD",
-//                                ordinal = 0 ),
-//                    ordinal = 0,
-//                    require = 1)
-//    private int modifyJ(int value) {
-//        int index = Arrays.binarySearch(InventoryDWS.HOTBAR_SLOTS, this.thePlayer.inventory.currentItem);
-//        return this.thePlayer.inventoryContainer.inventorySlots.size() - 18 + index;
-//    }
 }
