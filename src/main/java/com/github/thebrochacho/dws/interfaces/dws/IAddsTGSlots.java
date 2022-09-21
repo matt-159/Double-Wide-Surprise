44e184ddcdb5318c502f37be97af896b83e888b4
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-21 02:22:45 -0700

    added travellers gear slots

--- src/main/java/com/github/thebrochacho/dws/interfaces/dws/IAddsTGSlots.java
+++ src/main/java/com/github/thebrochacho/dws/interfaces/dws/IAddsTGSlots.java
@@ -1,5 +1,11 @@
 package com.github.thebrochacho.dws.interfaces.dws;
 
-public interface IAddsTGSlots {
+import net.minecraft.item.ItemStack;
 
+public interface IAddsTGSlots {
+    public int getTGSlotStart();
+
+    public ItemStack[] getTravellersAccessoriesItemStacks();
+
+    void setTravellersGearAccessories(ItemStack[] playerTGAccessories);
 }
