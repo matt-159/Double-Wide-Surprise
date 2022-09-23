674f8531300a2ccf9f98bea021cace98a4bae372
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-23 09:29:48 -0700

    removed null slots

--- src/main/java/com/github/thebrochacho/dws/interfaces/dws/IAddsNullSlots.java
+++
@@ -1,9 +0,0 @@
-package com.github.thebrochacho.dws.interfaces.dws;
-
-import org.apache.commons.lang3.tuple.Pair;
-
-import java.util.ArrayList;
-
-public interface IAddsNullSlots {
-    ArrayList<Pair<Integer, Integer>> getNullSlots();
-}
