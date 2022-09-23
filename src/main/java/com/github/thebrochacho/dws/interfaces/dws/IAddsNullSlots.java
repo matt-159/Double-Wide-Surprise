fb8e85d8e81aa64fb107e945c07d254af30a5a49
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-22 17:54:28 -0700

    removed redundant public modifier

--- src/main/java/com/github/thebrochacho/dws/interfaces/dws/IAddsNullSlots.java
+++ src/main/java/com/github/thebrochacho/dws/interfaces/dws/IAddsNullSlots.java
@@ -5,5 +5,5 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.ArrayList;
 
 public interface IAddsNullSlots {
-    public ArrayList<Pair<Integer, Integer>> getNullSlots();
+    ArrayList<Pair<Integer, Integer>> getNullSlots();
 }
