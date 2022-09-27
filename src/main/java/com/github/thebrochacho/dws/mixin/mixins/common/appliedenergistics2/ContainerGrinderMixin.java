4a7c292b12382a2f7abc5efa105f1fce3cc7d03e
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-27 11:28:54 -0700

    finished grindstone compat

---
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/appliedenergistics2/ContainerGrinderMixin.java
@@ -0,0 +1,23 @@
+package com.github.thebrochacho.dws.mixin.mixins.common.appliedenergistics2;
+
+import appeng.container.implementations.ContainerGrinder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerGrinder.class)
+public abstract class ContainerGrinderMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = { @Constant(intValue = 12),
+                                 @Constant(intValue = 12 + 18),
+                                 @Constant(intValue = 12 + 36),
+                                 @Constant(intValue = 80),
+                                 @Constant(intValue = 112),
+                                 @Constant(intValue = 112 + 18),
+                                 @Constant(intValue = 112 + 36) },
+                    remap = false,
+                    require = 7)
+    private int modifySlotXOffset(int constant) {
+        return constant + 81;
+    }
+}
