7672b0f6cfe995fae35c8a6c7a3f0b501ce5b21c
Matt <16054364+matt-159@users.noreply.github.com>
2022-09-29 22:53:37 -0700

    finished compat for the skystone chests

---
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/appliedenergistics2/ContainerSkyChestMixin.java
@@ -0,0 +1,17 @@
+package com.github.thebrochacho.dws.mixin.mixins.common.appliedenergistics2;
+
+import appeng.container.implementations.ContainerSkyChest;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerSkyChest.class)
+public abstract class ContainerSkyChestMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 8),
+                    remap = false,
+                    require = 1)
+    private int modifySlotXOffset(int constant) {
+        return constant + 81;
+    }
+}
