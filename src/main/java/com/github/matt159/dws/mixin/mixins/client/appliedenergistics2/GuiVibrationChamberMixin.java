5919af55b3e607f04284eaa88d4647e2b2a7f865
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-09-30 01:23:36 -0700

    finished compat for the vibration chamber

---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/appliedenergistics2/GuiVibrationChamberMixin.java
@@ -0,0 +1,29 @@
+package com.github.matt159.dws.mixin.mixins.client.appliedenergistics2;
+
+import appeng.client.gui.implementations.GuiVibrationChamber;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GuiVibrationChamber.class)
+public abstract class GuiVibrationChamberMixin {
+    @ModifyConstant(method = { "initGui",
+                               "drawFG" },
+                    constant = @Constant(intValue = 176),
+                    remap = false,
+                    require = 2)
+    private int modifyTextureUVXOffset(int constant) {
+        return 338;
+    }
+
+    @ModifyConstant(method = { "initGui",
+                               "drawFG",
+                               "drawBG" },
+                    constant = { @Constant(intValue = 81),
+                                 @Constant(intValue = 99) },
+                    remap = false,
+                    require = 3)
+    private int modifyProgressBarXOffset(int constant) {
+        return constant + 81;
+    }
+}
