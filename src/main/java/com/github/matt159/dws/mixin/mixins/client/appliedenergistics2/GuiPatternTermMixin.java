9242a88b5881336e1def0314b548812ee1df3afc
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-09-29 19:53:37 -0700

    finished compat for the pattern terminal

---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/appliedenergistics2/GuiPatternTermMixin.java
@@ -0,0 +1,30 @@
+package com.github.matt159.dws.mixin.mixins.client.appliedenergistics2;
+
+import appeng.client.gui.implementations.GuiPatternTerm;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GuiPatternTerm.class)
+public abstract class GuiPatternTermMixin {
+    @ModifyArg(method = "initGui",
+               at = @At(value = "INVOKE",
+                        target = "Lappeng/client/gui/widgets/GuiImgButton;<init>(IILjava/lang/Enum;Ljava/lang/Enum;)V"),
+               index = 0,
+               remap = false,
+               require = 1)
+    private int modifyGuiImgButtonXOffsets(int arg) {
+        return arg + 81;
+    }
+
+    @ModifyArg(method = "initGui",
+               at = @At(value = "INVOKE",
+                        target = "Lappeng/client/gui/widgets/GuiTabButton;<init>(IILnet/minecraft/item/ItemStack;Ljava/lang/String;Lnet/minecraft/client/renderer/entity/RenderItem;)V"),
+               index = 0,
+               remap = false,
+               require = 1)
+    private int modifyGuiTabButtonXOffset(int arg) {
+        return arg + 81;
+    }
+}
