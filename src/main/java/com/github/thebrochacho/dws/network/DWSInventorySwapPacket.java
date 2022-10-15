c93be8a0539faf612befe2ba7983a3779b954f82
Matt <16054364+matt-159@users.noreply.github.com>
2022-10-15 06:21:13 -0700

    modified swap packet and shiftMainInventory() signatures

--- src/main/java/com/github/thebrochacho/dws/network/DWSInventorySwapPacket.java
+++ src/main/java/com/github/thebrochacho/dws/network/DWSInventorySwapPacket.java
@@ -5,24 +5,29 @@ import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.entity.player.EntityPlayer;
 
 public class DWSInventorySwapPacket implements IMessage, IMessageHandler<DWSInventorySwapPacket, IMessage> {
-    public DWSInventorySwapPacket() {}
+    boolean needsFallbackSupport;
 
-    public DWSInventorySwapPacket(EntityPlayer player) {}
+    public DWSInventorySwapPacket() {
+        this(false);
+    }
+
+    public DWSInventorySwapPacket(boolean needsFallbackSupport) {
+        this.needsFallbackSupport = needsFallbackSupport;
+    }
 
     public void fromBytes(ByteBuf buf) {
-        //extract extra data from the packet here
+       this.needsFallbackSupport = buf.readBoolean();
     }
 
     public void toBytes(ByteBuf buf) {
+        buf.writeBoolean(this.needsFallbackSupport);
     }
 
     @Override
     public IMessage onMessage(DWSInventorySwapPacket packet, MessageContext context) {
-        //handle incoming packets here. context.side is CLIENT if this packet was RECEIVED by the client, and SERVER if the packet was RECEIVED by the server
-        DWSUtil.shiftMainInventory(context.getServerHandler().playerEntity);
+        DWSUtil.shiftMainInventory(context.getServerHandler().playerEntity, packet.needsFallbackSupport);
         return null;
     }
 }
