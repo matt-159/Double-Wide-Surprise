1f220a7942826111ced78040541b57c990c46372
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-10-15 06:19:16 -0700

    player inventory gets rearranged upon closing an unsupported inventory

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerMixin.java
@@ -1,9 +1,15 @@
 package com.github.matt159.dws.mixin.mixins.common.minecraft.inventory;
 
+import com.github.matt159.dws.interfaces.minecraft.IEntityPlayerMixin;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.ContainerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Container.class)
 public abstract class ContainerMixin {
@@ -12,5 +18,18 @@ public abstract class ContainerMixin {
                     constant = @Constant(intValue = 9))
     private int modifyHotbarRangeCheck(int constant) {
         return 18;
+    }
+
+    @Inject(method = "onContainerClosed",
+            at = @At("RETURN"),
+            require = 1)
+    private void injectPlayerInventoryReorganization(EntityPlayer player, CallbackInfo ci) {
+        if (((Container) (Object) this) instanceof ContainerPlayer) {
+            return;
+        }
+
+        if (((IEntityPlayerMixin) player).isInventoryReorganized()) {
+            ((IEntityPlayerMixin) player).resetInventory();
+        }
     }
 }
