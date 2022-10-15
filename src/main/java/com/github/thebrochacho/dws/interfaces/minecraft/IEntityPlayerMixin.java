5c5f5334de150f66d2026597ab99add74e60949d
Matt <16054364+matt-159@users.noreply.github.com>
2022-10-15 06:17:16 -0700

    player inventory gets rearranged upon opening an unsupported inventory

---
+++ src/main/java/com/github/thebrochacho/dws/interfaces/minecraft/IEntityPlayerMixin.java
@@ -0,0 +1,7 @@
+package com.github.thebrochacho.dws.interfaces.minecraft;
+
+public interface IEntityPlayerMixin {
+    boolean isInventoryReorganized();
+
+    void resetInventory();
+}
--- src/main/java/com/github/thebrochacho/dws/util/DWSUtil.java
+++ src/main/java/com/github/thebrochacho/dws/util/DWSUtil.java
@@ -41,8 +41,102 @@ public final class DWSUtil {
         Tessellator.instance.draw();
     }
 
-    public static void RearrangeInventoryForFallback(EntityPlayer player) {
+    public enum Reorganization {
+        Do, Undo
+    }
 
+    public static void ReorganizeInventoryForFallbackSupport(EntityPlayer player, Reorganization operation) {
+        if (player.worldObj.isRemote) {
+            return;
+        }
+
+        InventoryPlayer inventory = new InventoryPlayer(player);
+        inventory.copyInventory(player.inventory);
+
+        int size = inventory.mainInventory.length;
+        List<ItemStack> items = new ArrayList<>(size);
+
+        switch (operation) {
+            case Do:
+                /*
+                *   ────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────
+                *  │ 18 │ 19 │ 20 │ 21 │ 22 │ 23 │ 24 │ 25 │ 26 │ 27 │ 28 │ 29 │ 30 │ 31 │ 32 │ 33 │ 34 │ 35 │
+                *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                *  │ 36 │ 37 │ 38 │ 39 │ 40 │ 41 │ 42 │ 43 │ 44 │ 45 │ 46 │ 47 │ 48 │ 49 │ 50 │ 51 │ 52 │ 53 │
+                *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                *  │ 54 │ 55 │ 56 │ 57 │ 58 │ 59 │ 60 │ 61 │ 62 │ 63 │ 64 │ 65 │ 66 │ 67 │ 68 │ 69 │ 70 │ 71 │
+                *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                *  │ 0  │ 1  │ 2  │ 3  │ 4  │ 5  │ 6  │ 7  │ 8  │ 9  │ 10 │ 11 │ 12 │ 13 │ 14 │ 15 │ 16 │ 17 │
+                *   ────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────
+                *                                               ↓
+                *   ────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────
+                *  │ 36 │ 37 │ 38 │ 39 │ 40 │ 41 │ 42 │ 43 │ 44 │ 54 │ 55 │ 56 │ 57 │ 58 │ 59 │ 60 │ 61 │ 62 │
+                *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                *  │  9 │ 10 │ 11 │ 12 │ 13 │ 14 │ 15 │ 16 │ 17 │ 27 │ 28 │ 29 │ 30 │ 31 │ 32 │ 33 │ 34 │ 35 │
+                *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                *  │ 45 │ 46 │ 47 │ 48 │ 49 │ 50 │ 51 │ 52 │ 53 │ 63 │ 64 │ 65 │ 66 │ 67 │ 68 │ 69 │ 70 │ 71 │
+                *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                *  │  0 │  1 │  2 │  3 │  4 │  5 │  6 │  7 │  8 │ 18 │ 19 │ 20 │ 21 │ 22 │ 23 │ 24 │ 25 │ 26 │
+                *   ────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────
+                */
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 36, 45)); //matters
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 54, 63)); //matters
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 9, 18));  //doesn't matter
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 27, 36)); //doesn't matter
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 45, 54)); //doesn't matter
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 63, 72)); //doesn't matter
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 0, 9));   //matters
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 18, 27)); //matters
+
+                break;
+            case Undo:
+                /*
+                 *   ────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────
+                 *  │ 36 │ 37 │ 38 │ 39 │ 40 │ 41 │ 42 │ 43 │ 44 │ 54 │ 55 │ 56 │ 57 │ 58 │ 59 │ 60 │ 61 │ 62 │
+                 *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                 *  │  9 │ 10 │ 11 │ 12 │ 13 │ 14 │ 15 │ 16 │ 17 │ 27 │ 28 │ 29 │ 30 │ 31 │ 32 │ 33 │ 34 │ 35 │
+                 *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                 *  │ 45 │ 46 │ 47 │ 48 │ 49 │ 50 │ 51 │ 52 │ 53 │ 63 │ 64 │ 65 │ 66 │ 67 │ 68 │ 69 │ 70 │ 71 │
+                 *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                 *  │  0 │  1 │  2 │  3 │  4 │  5 │  6 │  7 │  8 │ 18 │ 19 │ 20 │ 21 │ 22 │ 23 │ 24 │ 25 │ 26 │
+                 *   ────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────
+                 *                                               ↓
+                 *   ────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────┬────
+                 *  │ 18 │ 19 │ 20 │ 21 │ 22 │ 23 │ 24 │ 25 │ 26 │ 27 │ 28 │ 29 │ 30 │ 31 │ 32 │ 33 │ 34 │ 35 │
+                 *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                 *  │ 36 │ 37 │ 38 │ 39 │ 40 │ 41 │ 42 │ 43 │ 44 │ 45 │ 46 │ 47 │ 48 │ 49 │ 50 │ 51 │ 52 │ 53 │
+                 *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                 *  │ 54 │ 55 │ 56 │ 57 │ 58 │ 59 │ 60 │ 61 │ 62 │ 63 │ 64 │ 65 │ 66 │ 67 │ 68 │ 69 │ 70 │ 71 │
+                 *  ├────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┼────┤
+                 *  │  0 │  1 │  2 │  3 │  4 │  5 │  6 │  7 │  8 │  9 │ 10 │ 11 │ 12 │ 13 │ 14 │ 15 │ 16 │ 17 │
+                 *   ────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────
+                 */
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 9, 18));
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 45, 54));
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 18, 27));
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 54, 63));
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 27, 36));
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 63, 72));
+
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 0, 9));
+                Collections.addAll(items, Arrays.copyOfRange(inventory.mainInventory, 36, 45));
+
+                break;
+        }
+
+        ItemStack[] itemStacks = items.toArray(new ItemStack[0]);
+
+        for (int i = 0; i < size; ++i) {
+            //i + 9 gets past the armor and crafting slots
+            player.inventoryContainer.putStackInSlot(i + 9, itemStacks[i]);
+        }
+        player.inventoryContainer.detectAndSendChanges();
     }
 
     public static void shiftMainInventory(EntityPlayer player, boolean needsFallbackSupport) {
