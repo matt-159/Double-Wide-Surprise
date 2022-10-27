b366556699af38428b80f983c6221bab64da87ee
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-10-27 06:58:31 -0700

    added list of compatible mods

--- src/main/java/com/github/matt159/dws/util/ModCompat.java
+++ src/main/java/com/github/matt159/dws/util/ModCompat.java
@@ -1,13 +1,52 @@
 package com.github.matt159.dws.util;
 
 import baubles.common.Baubles;
+import cpw.mods.fml.common.ModContainer;
 import micdoodle8.mods.galacticraft.core.Constants;
 import tconstruct.TConstruct;
 import travellersgear.TravellersGear;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static cpw.mods.fml.common.Loader.isModLoaded;
 
 public final class ModCompat {
+    enum CompatibleMods {
+        VANILLA("minecraft"),
+        AE2("appliedenergistics2"),
+        AE2_CORE("appliedenergistics2-core"),
+        GREGTECH("gregtech"),
+        IRONCHEST("IronChest"),
+        NEI("NotEnoughItems");
+
+        private final String modID;
+
+        CompatibleMods(String modID) {
+            this.modID = modID;
+        }
+
+        String modID() {
+            return this.modID;
+        }
+    }
+
+    private static List<String> modIDs = null;
+
+    public static List<String> getModIDs() {
+        if (modIDs == null) {
+            modIDs = Arrays.stream(CompatibleMods.values())
+                           .map(CompatibleMods::modID)
+                           .collect(Collectors.toList());
+        }
+        return modIDs;
+    }
+
+    public static boolean hasDWSCompat(ModContainer mc) {
+        return getModIDs().contains(mc.getModId());
+    }
+
     private ModCompat() {
         throw new IllegalArgumentException();
     }
