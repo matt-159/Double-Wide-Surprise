9e46ea67b47aaeed2c752937ede3c1f5379871bb
Matt <16054364+matt-159@users.noreply.github.com>
2022-11-01 03:34:18 -0700

    player inventory gets relocated correctly

--- src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/forestry/ContainerForestryMixin.java
+++ src/main/java/com/github/thebrochacho/dws/mixin/mixins/common/forestry/ContainerForestryMixin.java
@@ -2,8 +2,10 @@ package com.github.thebrochacho.dws.mixin.mixins.common.forestry;
 
 import forestry.core.gui.ContainerForestry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ContainerForestry.class)
 public abstract class ContainerForestryMixin {
@@ -13,5 +15,15 @@ public abstract class ContainerForestryMixin {
                     require = 1)
     private int modifyPlayerInventorySize(int constant) {
         return 18;
+    }
+
+    @ModifyVariable(method = "addPlayerInventory",
+                    at = @At("HEAD"),
+                    argsOnly = true,
+                    ordinal = 0,
+                    remap = false,
+                    require = 1)
+    private int modifyPlayerXOffset(int constant) {
+        return 8;
     }
 }
