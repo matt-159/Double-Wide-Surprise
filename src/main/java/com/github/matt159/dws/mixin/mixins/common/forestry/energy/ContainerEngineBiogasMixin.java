7dbd20f2f3fc8e32fd36f2b5590a030974c6fac4
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-11-06 23:35:59 -0800

    finished energy containers

---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/forestry/energy/ContainerEngineBiogasMixin.java
@@ -0,0 +1,17 @@
+package com.github.matt159.dws.mixin.mixins.common.forestry.energy;
+
+import forestry.energy.gui.ContainerEngineBiogas;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerEngineBiogas.class)
+public abstract class ContainerEngineBiogasMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 143),
+                    remap = false,
+                    require = 1)
+    private int modifySlotXOffset(int constant) {
+        return constant + 81;
+    }
+}
---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/forestry/energy/ContainerEnginePeatMixin.java
@@ -0,0 +1,19 @@
+package com.github.matt159.dws.mixin.mixins.common.forestry.energy;
+
+import forestry.energy.gui.ContainerEnginePeat;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerEnginePeat.class)
+public abstract class ContainerEnginePeatMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = { @Constant(intValue = 44),
+                                 @Constant(intValue = 98),
+                                 @Constant(intValue = 116) },
+                    remap = false,
+                    require = 1)
+    private int modifySlotXOffset(int constant) {
+        return constant + 81;
+    }
+}
