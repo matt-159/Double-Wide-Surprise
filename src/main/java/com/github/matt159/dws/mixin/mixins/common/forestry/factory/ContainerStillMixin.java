2ae2dcddf19bb513c84d57e4095f44412d6ea0d0
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-11-09 07:40:05 -0800

    finished Factory containers

---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/forestry/factory/ContainerStillMixin.java
@@ -0,0 +1,18 @@
+package com.github.matt159.dws.mixin.mixins.common.forestry.factory;
+
+import forestry.factory.gui.ContainerStill;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerStill.class)
+public abstract class ContainerStillMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = { @Constant(intValue = 10),
+                                 @Constant(intValue = 150) },
+                    remap = false,
+                    require = 3)
+    private int modifySlotXOffset(int constant) {
+        return constant + 81;
+    }
+}
---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/forestry/factory/ContainerWorktableMixin.java
@@ -0,0 +1,20 @@
+package com.github.matt159.dws.mixin.mixins.common.forestry.factory;
+
+import forestry.factory.gui.ContainerWorktable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ContainerWorktable.class)
+public abstract class ContainerWorktableMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = { @Constant(intValue = 8,
+                                           ordinal = 1),
+                                 @Constant(intValue = 11),
+                                 @Constant(intValue = 77) },
+                    remap = false,
+                    require = 3)
+    private int modifySlotXOffset(int constant) {
+        return constant + 81;
+    }
+}
