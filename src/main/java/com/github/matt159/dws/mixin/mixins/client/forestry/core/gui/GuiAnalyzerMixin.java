cfc9d6ede3be5776f976bc0e26db3bcb558ed5ec
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-11-26 05:30:41 -0800

    made gui sub-packages

---
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/forestry/core/gui/GuiAnalyzerMixin.java
@@ -0,0 +1,25 @@
+package com.github.matt159.dws.mixin.mixins.client.forestry.core.gui;
+
+import forestry.core.gui.GuiAnalyzer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(GuiAnalyzer.class)
+public abstract class GuiAnalyzerMixin {
+    @ModifyConstant(method = "<init>",
+                    constant = @Constant(intValue = 95),
+                    remap = false,
+                    require = 1)
+    private int modifyTankXOffset(int constant) {
+        return constant + 81;
+    }
+
+    @ModifyConstant(method = "drawGuiContainerBackgroundLayer",
+                    constant = @Constant(intValue = 64),
+                    remap = false,
+                    require = 1)
+    private int modifyProgressBarXOffset(int constant) {
+        return constant + 81;
+    }
+}
