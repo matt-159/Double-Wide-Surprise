643bbba1b59afc953a5dfd3961bf03a3088e4d13
Matt <16054364+matt-159@users.noreply.github.com>
2022-12-21 21:32:31 -0800

    optimized imports

--- src/main/java/com/github/matt159/dws/mixin/mixins/client/inventorytweaks/VanillaSlotMapsMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/inventorytweaks/VanillaSlotMapsMixin.java
@@ -1,15 +1,9 @@
 package com.github.matt159.dws.mixin.mixins.client.inventorytweaks;
 
-import com.github.matt159.dws.util.DWSUtil;
 import invtweaks.containers.VanillaSlotMaps;
-import net.minecraft.inventory.Slot;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.List;
 
 @Mixin(VanillaSlotMaps.class)
 public abstract class VanillaSlotMapsMixin {
