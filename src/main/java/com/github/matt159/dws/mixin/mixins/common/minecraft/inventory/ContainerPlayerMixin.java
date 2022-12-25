5182250346a464a212e9188205219dda5bad14b3
matt-159 <16054364+matt-159@users.noreply.github.com>
2022-12-25 02:26:00 -0800

    baubles items can be shift-clicked into their slots

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerPlayerMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerPlayerMixin.java
@@ -1,17 +1,23 @@
 package com.github.matt159.dws.mixin.mixins.common.minecraft.inventory;
 
+import baubles.api.BaubleType;
+import baubles.api.IBauble;
 import com.github.matt159.dws.interfaces.dws.IAddsBaubleSlots;
-import com.github.matt159.dws.interfaces.dws.IAddsGCSlots;
-import com.github.matt159.dws.interfaces.dws.IAddsTGSlots;
-import com.github.matt159.dws.interfaces.dws.IAddsTinkersSlots;
+import com.github.matt159.dws.util.ModCompat;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerPlayer;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ContainerPlayer.class)
 public abstract class ContainerPlayerMixin extends Container  {
+
+    private boolean didMerge = false;
 
     @ModifyConstant(method = "<init>",
                     constant = {    @Constant(intValue = 88),
@@ -40,5 +46,78 @@ public abstract class ContainerPlayerMixin extends Container  {
                     require = 6)
     private int modifyHotbarSlotEnd(int constant) {
         return 81;
+    }
+
+    @Inject(method = "transferStackInSlot",
+            at = { @At(value = "INVOKE",
+                       target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
+                       ordinal = 4),
+                   @At(value = "INVOKE",
+                       target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
+                       ordinal = 5) },
+            cancellable = true,
+            require = 2)
+    private void injectAccessoryShiftClickAttempt(EntityPlayer player, int slotNumber, CallbackInfoReturnable<ItemStack> cir) {
+        this.didMerge = false;
+        ItemStack itemstack1 = ((Slot) this.inventorySlots.get(slotNumber)).getStack();
+        ItemStack itemstack = itemstack1.copy();
+
+        int startIndex;
+        int endIndex = -1;
+
+        if (ModCompat.isBaublesPresent() && itemstack.getItem() instanceof IBauble) {
+            IBauble bauble = (IBauble) itemstack.getItem();
+            BaubleType type = bauble.getBaubleType(itemstack);
+
+            startIndex = ((IAddsBaubleSlots) this).getBaublesSlotStart();
+
+            switch (type) {
+                case RING:
+                    startIndex += 1;
+                    endIndex = startIndex + 2;
+                    break;
+                case BELT:
+                    startIndex += 3;
+                case AMULET:
+                    endIndex = startIndex + 1;
+                    break;
+            }
+
+            if (bauble.canEquip(itemstack, player) && emptySlotInRange(startIndex, endIndex)) {
+                if (!mergeItemStack(itemstack1, startIndex, endIndex, false)) {
+                    cir.setReturnValue(null);
+                    cir.cancel();
+                }
+                didMerge = true;
+            }
+        } else if (ModCompat.isTinkersConstructPresent()) {
+
+        } else if (ModCompat.isTravellersGearPresent()) {
+
+        } else if (ModCompat.isGalacticraftPresent()) {
+
+        }
+    }
+
+    @ModifyExpressionValue(method = "transferStackInSlot",
+                           at = { @At(value = "INVOKE",
+                                      target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
+                                      ordinal = 4),
+                                  @At(value = "INVOKE",
+                                      target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
+                                      ordinal = 5) },
+                           require = 2)
+    private boolean skipMergeCheck(boolean original) {
+        return this.didMerge || original;
+    }
+
+    private boolean emptySlotInRange(int startIndex, int endIndex) {
+        boolean ret = false;
+
+        for (; startIndex < endIndex; startIndex++) {
+            ret = ret || !((Slot) this.inventorySlots.get(startIndex)).getHasStack();
+        }
+
+        return ret;
     }
 }
