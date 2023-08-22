b7db80116b29d9aa6154d66f3bfe9bd8bb01413b
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-08-21 20:04:36 -0700

    add marker interface

---
+++ src/main/java/com/github/matt159/dws/interfaces/IDWSContainer.java
@@ -0,0 +1,8 @@
+package com.github.matt159.dws.interfaces;
+
+/**
+ * Marker interface for a supported.
+ * A container that implements this interface will not be shuffled before its corresponding gui is opened
+ */
+public interface IDWSContainer {
+}
