cdeccb48284c454e47b5ca9b561ae6d036804401
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-09-03 04:22:39 -0700

    s p a c i n g

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerChestMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerChestMixin.java
@@ -13,19 +13,18 @@ import org.spongepowered.asm.mixin.injection.Slice;
 public abstract class ContainerChestMixin extends Container {
 
     @ModifyConstant(method = "<init>",
-                    constant = @Constant(   intValue = 8,
-                                            ordinal = 0),
+                    constant = @Constant(intValue = 8, ordinal = 0),
                     require = 1)
     private int modifyChestSlotsXOffset(int constant) {
         return constant + Constants.GENERAL_X_OFFSET;
     }
 
     @ModifyConstant(method = "<init>",
-                    slice = @Slice( from =  @At(value = "INVOKE",
-                                                target = "Lnet/minecraft/inventory/ContainerChest;addSlotToContainer(Lnet/minecraft/inventory/Slot;)Lnet/minecraft/inventory/Slot;",
-                                                ordinal = 0,
-                                                shift = At.Shift.AFTER),
-                                    to =    @At("RETURN")),
+                    slice = @Slice(from = @At(value = "INVOKE",
+                                              target = "Lnet/minecraft/inventory/ContainerChest;addSlotToContainer(Lnet/minecraft/inventory/Slot;)Lnet/minecraft/inventory/Slot;",
+                                              ordinal = 0,
+                                              shift = At.Shift.AFTER),
+                                   to = @At("RETURN")),
                     constant = @Constant(intValue = 9),
                     require = 3)
     private int modifyPlayerInventorySlotCount(int constant) {
