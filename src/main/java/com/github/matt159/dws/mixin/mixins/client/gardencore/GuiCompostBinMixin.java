fab38a59045b12effff43ed2b408f74580c59dd9
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-09-03 04:22:03 -0700

    clean up mixin class

--- src/main/java/com/github/matt159/dws/mixin/mixins/client/gardencore/GuiCompostBinMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/gardencore/GuiCompostBinMixin.java
@@ -1,29 +1,18 @@
 package com.github.matt159.dws.mixin.mixins.client.gardencore;
 
+import com.github.matt159.dws.interfaces.IDWSGui;
 import com.github.matt159.dws.util.Constants;
-import com.jaquadro.minecraft.gardencore.block.tile.TileEntityCompostBin;
 import com.jaquadro.minecraft.gardencore.client.gui.GuiCompostBin;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiCompostBin.class)
-public abstract class GuiCompostBinMixin extends GuiContainer {
+public abstract class GuiCompostBinMixin extends GuiContainer implements IDWSGui {
     public GuiCompostBinMixin(Container container) {
         super(container);
-    }
-
-    @Inject(method = "<init>",
-            at = @At("RETURN"),
-            require = 1)
-    private void injectNewXSize(InventoryPlayer inventory, TileEntityCompostBin tileEntity, CallbackInfo ci) {
-        this.xSize = Constants.GENERAL_DWS_GUI_WIDTH;
     }
 
     @ModifyConstant(method = "drawGuiContainerBackgroundLayer",
