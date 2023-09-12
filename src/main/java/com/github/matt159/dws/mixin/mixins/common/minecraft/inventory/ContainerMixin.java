e5762aa5e52408eb22e5eecf802b6667fc1fb0fd
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-09-12 10:21:34 -0700

    simplify transferStackInSlot mixin

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerMixin.java
@@ -3,22 +3,14 @@ package com.github.matt159.dws.mixin.mixins.common.minecraft.inventory;
 import com.github.matt159.dws.interfaces.minecraft.IEntityPlayerMixin;
 import com.github.matt159.dws.util.ReflectedModSupport;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerPlayer;
-import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.lang.ref.WeakReference;
 
 @Mixin(Container.class)
 public abstract class ContainerMixin {
@@ -41,28 +33,5 @@ public abstract class ContainerMixin {
         if (((IEntityPlayerMixin) player).isInventoryReorganized()) {
             ((IEntityPlayerMixin) player).resetInventory();
         }
-    }
-
-    @Unique
-    private WeakReference<ItemStack> dws$capturedItemStack = null;
-
-    @Inject(method = "mergeItemStack",
-            at = @At("HEAD"),
-            require = 1)
-    private void injectItemStackCapture(ItemStack itemStack,
-                                        int start,
-                                        int end,
-                                        boolean scanReverse,
-                                        CallbackInfoReturnable<Boolean> cir) {
-        this.dws$capturedItemStack = new WeakReference<>(itemStack);
-    }
-
-    @Redirect(method = "mergeItemStack",
-              at = @At(value = "INVOKE",
-                       target = "Lnet/minecraft/inventory/Slot;getStack()Lnet/minecraft/item/ItemStack;",
-                       ordinal = 1),
-              require = 1)
-    private ItemStack captureSlot(Slot instance) {
-        return instance.isItemValid(this.dws$capturedItemStack.get()) ? instance.getStack() : new ItemStack(Blocks.air);
     }
 }
--- src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerPlayerMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/minecraft/inventory/ContainerPlayerMixin.java
@@ -1,20 +1,17 @@
 package com.github.matt159.dws.mixin.mixins.common.minecraft.inventory;
 
 import com.github.matt159.dws.util.SlotLayoutManager;
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.entity.player.EntityPlayer;
+import lombok.val;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerPlayer;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ContainerPlayer.class)
 public abstract class ContainerPlayerMixin extends Container  {
-    private boolean didMerge = false;
-
     @ModifyConstant(method = "<init>",
                     constant = {    @Constant(intValue = 88),
                                     @Constant(intValue = 144)   },
@@ -44,139 +41,50 @@ public abstract class ContainerPlayerMixin extends Container  {
         return 81;
     }
 
-    @Inject(method = "transferStackInSlot",
-            at = { @At(value = "INVOKE",
+    @Redirect(method = "transferStackInSlot",
+              at = @At(value = "INVOKE",
                        target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
                        ordinal = 4),
-                   @At(value = "INVOKE",
-                       target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
-                       ordinal = 5) },
-            cancellable = true,
-            require = 2)
-    private void injectTryShiftClickIntoAccessorySlot(EntityPlayer player, int slotNumber, CallbackInfoReturnable<ItemStack> cir) {
-        this.didMerge = false;
-        ItemStack itemstack1 = ((Slot) this.inventorySlots.get(slotNumber)).getStack();
-        ItemStack itemstack = itemstack1.copy();
+              require = 1)
+    private boolean tryMergeFromMainIntoAccessorySlots(ContainerPlayer instance,
+                                               ItemStack itemStack,
+                                               int start,
+                                               int end,
+                                               boolean scanReverse) {
+        boolean didMerge = this.dws$mergeIntoAccessorySlots(itemStack);
 
-        int startIndex = SlotLayoutManager.getFirstAccessorySlot();
-        int endIndex = this.inventorySlots.size();
+        return didMerge || this.mergeItemStack(itemStack, start, end, scanReverse);
+    }
 
-        if (!mergeItemStack(itemstack1, startIndex, endIndex, false)) {
-            cir.setReturnValue(null);
-            cir.cancel();
-            return;
+    @Redirect(method = "transferStackInSlot",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
+                    ordinal = 5),
+            require = 1)
+    private boolean tryMergeFromHotbarIntoAccessorySlots(ContainerPlayer instance,
+                                                         ItemStack itemStack,
+                                                         int start,
+                                                         int end,
+                                                         boolean scanReverse) {
+        boolean didMerge = this.dws$mergeIntoAccessorySlots(itemStack);
+
+        return didMerge || this.mergeItemStack(itemStack, start, end, scanReverse);
+    }
+
+    @Unique
+    private boolean dws$mergeIntoAccessorySlots(ItemStack itemStack) {
+        val startIndex = SlotLayoutManager.getFirstAccessorySlot();
+        val endIndex = this.inventorySlots.size();
+
+        for (int index = startIndex; index < endIndex; index++) {
+            val slot = (Slot) this.inventorySlots.get(index);
+
+            if (slot.isItemValid(itemStack)) {
+                if (mergeItemStack(itemStack, index, index + 1, false)) {
+                    return true;
+                }
+            }
         }
-        this.didMerge = true;
-
-//        if (ModCompat.isBaublesPresent() && ReflectedModSupport.instanceof_IBauble(itemstack.getItem())) {
-//            if (((IBauble) itemstack.getItem()).getBaubleType(itemstack) == null) return;
-//
-//            IBauble bauble = (IBauble) itemstack.getItem();
-//            BaubleType type = bauble.getBaubleType(itemstack);
-//
-//            startIndex = ((IBaubleManager) this).getBaublesSlotStart();
-//
-//            switch (type) {
-//                case RING:
-//                    startIndex += 1;
-//                    endIndex = startIndex + 2;
-//                    break;
-//                case BELT:
-//                    startIndex += 3;
-//                case AMULET:
-//                    endIndex = startIndex + 1;
-//                    break;
-//            }
-//
-//            if (bauble.canEquip(itemstack, player) && emptySlotInRange(startIndex, endIndex)) {
-//                if (!mergeItemStack(itemstack1, startIndex, endIndex, false)) {
-//                    cir.setReturnValue(null);
-//                    cir.cancel();
-//                    return;
-//                }
-//                didMerge = true;
-//            }
-//        } else if (ModCompat.isTinkersConstructPresent() &&
-//                   ReflectedModSupport.instanceof_IAccessory(itemstack.getItem())) {
-//            startIndex = SlotLayoutManager.FIRST_TINKERS_SLOT;
-//
-//            int i;
-//            for (i = 0; i < 7; i++) {
-//                if (ReflectedModSupport.IAccessory_canEquipAccessory(itemstack, i)) {
-//                    break;
-//                }
-//            }
-//
-//            if (i == 7) {
-//                cir.setReturnValue(null);
-//                cir.cancel();
-//                return;
-//            }
-//
-//            startIndex += TinkersSlotMappings[i];
-//            endIndex = startIndex + 1;
-//
-//            if (!mergeItemStack(itemstack1, startIndex, endIndex, false)) {
-//                cir.setReturnValue(null);
-//                cir.cancel();
-//                return;
-//            }
-//            didMerge = true;
-//        } else if (ModCompat.isTravellersGearPresent() && ReflectedModSupport.instanceof_ITravellersGear(itemstack.getItem())) {
-//            startIndex = SlotLayoutManager.FIRST_TRAVELLERS_GEAR_SLOT + ReflectedModSupport.ITravellersGear_getSlot(itemstack);
-//            endIndex = startIndex + 1;
-//
-//            if (!mergeItemStack(itemstack1, startIndex, endIndex, false)) {
-//                cir.setReturnValue(null);
-//                cir.cancel();
-//                return;
-//            }
-//            didMerge = true;
-//        } else if (ModCompat.isGalacticraftPresent() && itemstack.getItem() instanceof IGalacticWearable) {
-//            startIndex = SlotLayoutManager.FIRST_GALACTICRAFT_SLOT;
-//
-//            int i;
-//            for (i = 0; i < 10; i++) {
-//                Slot slot = (Slot) this.inventorySlots.get(startIndex + i);
-//                if (slot.isItemValid(itemstack) && !slot.getHasStack()) {
-//                    break;
-//                }
-//            }
-//
-//            startIndex += i;
-//            endIndex = startIndex + 1;
-//
-//            if (!mergeItemStack(itemstack1, startIndex, endIndex, false)) {
-//                cir.setReturnValue(null);
-//                cir.cancel();
-//                return;
-//            }
-//            didMerge = true;
-//        }
+        return false;
     }
-
-    @ModifyExpressionValue(method = "transferStackInSlot",
-                           at = { @At(value = "INVOKE",
-                                      target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
-                                      ordinal = 4),
-                                  @At(value = "INVOKE",
-                                      target = "Lnet/minecraft/inventory/ContainerPlayer;mergeItemStack(Lnet/minecraft/item/ItemStack;IIZ)Z",
-                                      ordinal = 5) },
-                           require = 2)
-    private boolean skipMergeCheck(boolean original) {
-        return this.didMerge || original;
-    }
-
-    private boolean emptySlotInRange(int startIndex, int endIndex) {
-        boolean ret = false;
-
-        for (; startIndex < endIndex; startIndex++) {
-            ret = ret || !((Slot) this.inventorySlots.get(startIndex)).getHasStack();
-        }
-
-        return ret;
-    }
-
-    //Have this janky shit because of how I've ordered the tinkers slots
-    private static final int[] TinkersSlotMappings = new int[] {0, 1, 3, 2, 6, 5, 4};
 }
