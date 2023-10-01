d42dc3350055ad6af1a7cf46994d76c7161acc78
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-10-01 02:20:20 -0700

    add KeyHandler class

---
+++ src/main/java/com/github/matt159/dws/events/keybinds/KeyHandler.java
@@ -0,0 +1,27 @@
+package com.github.matt159.dws.events.keybinds;
+
+import net.minecraft.client.settings.KeyBinding;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+
+public class KeyHandler extends KeyBinding {
+    private final CallBack callBack;
+
+    public KeyHandler(String description, int keyCode, String category, CallBack callback) {
+        super(description, keyCode, category);
+        this.callBack = callback;
+        FMLCommonHandler.instance().bus().register(this);
+    }
+
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        if (this.isPressed()) {
+            callBack.onPress();
+        }
+    }
+
+    public interface CallBack {
+        void onPress();
+    }
+}
