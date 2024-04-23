f1f70869d5e61b8b39865a08c7b2518ec6c69153
matt-159 <16054364+matt-159@users.noreply.github.com>
2024-04-22 20:05:23 -0700

    tidy pass

--- src/main/java/com/github/matt159/dws/mixin/mixins/client/minecraft/TextureManagerMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/client/minecraft/TextureManagerMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.renderer.texture.ITextureObject;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.util.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -15,44 +16,46 @@ import java.util.Map;
 
 @Mixin(TextureManager.class)
 public abstract class TextureManagerMixin {
-
-    private ResourceLocation rl;
+    @Unique
+    private ResourceLocation $DWS_resourceLocation;
 
     @Inject(method = "bindTexture",
             at = @At("HEAD"),
             require = 1)
     private void injectNewResourceLocation(ResourceLocation resourceLocation, CallbackInfo ci) {
-        rl = TextureWhitelist.checkResourceLocation(resourceLocation);
+        this.$DWS_resourceLocation = TextureWhitelist.checkResourceLocation(resourceLocation);
     }
 
-    @Redirect(  method = "bindTexture",
-                at = @At(   value = "INVOKE",
-                            target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"),
-                require = 1)
+    @Redirect(method = "bindTexture",
+              at = @At(value = "INVOKE",
+                       target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"),
+              require = 1)
     private Object modifyMapGetArg(Map instance, Object o) {
-        return instance.get(rl);
+        return instance.get($DWS_resourceLocation);
     }
 
-    @ModifyArg( method = "bindTexture",
-                at = @At(   value = "INVOKE",
-                            target = "Lnet/minecraft/client/renderer/texture/SimpleTexture;<init>(Lnet/minecraft/util/ResourceLocation;)V"),
-                require = 1)
-    private ResourceLocation modifySimpleTextureArg(ResourceLocation p_i1275_1_) {
-        return rl;
+    @ModifyArg(method = "bindTexture",
+               at = @At(value = "INVOKE",
+                        target = "Lnet/minecraft/client/renderer/texture/SimpleTexture;<init>(Lnet/minecraft/util/ResourceLocation;)V"),
+               require = 1)
+    private ResourceLocation modifySimpleTextureArg(ResourceLocation resourceLocation) {
+        return this.$DWS_resourceLocation;
     }
 
-    @Redirect(  method = "bindTexture",
-                at = @At(   value = "INVOKE",
-                            target = "Lnet/minecraft/client/renderer/texture/TextureManager;loadTexture(Lnet/minecraft/util/ResourceLocation;Lnet/minecraft/client/renderer/texture/ITextureObject;)Z"),
-                require = 1)
-    private boolean modifyLoadTextureArg(TextureManager instance, ResourceLocation resourceLocation, ITextureObject iTextureObject) {
-        return instance.loadTexture(rl, iTextureObject);
+    @Redirect(method = "bindTexture",
+              at = @At(value = "INVOKE",
+                       target = "Lnet/minecraft/client/renderer/texture/TextureManager;loadTexture(Lnet/minecraft/util/ResourceLocation;Lnet/minecraft/client/renderer/texture/ITextureObject;)Z"),
+              require = 1)
+    private boolean modifyLoadTextureArg(TextureManager instance,
+                                         ResourceLocation resourceLocation,
+                                         ITextureObject iTextureObject) {
+        return instance.loadTexture(this.$DWS_resourceLocation, iTextureObject);
     }
 
     @Inject(method = "bindTexture",
             at = @At("RETURN"),
             require = 1)
-    private void injectFreeResourceLocation(ResourceLocation p_110577_1_, CallbackInfo ci) {
-        rl = null;
+    private void injectFreeResourceLocation(ResourceLocation resourceLocation, CallbackInfo ci) {
+        $DWS_resourceLocation = null;
     }
 }
