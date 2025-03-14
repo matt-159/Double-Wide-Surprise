f339fdcabb894d95a17c04677e9867601bee5ed0
matt-159 <16054364+matt-159@users.noreply.github.com>
2025-03-13 23:17:39 -0400

    simplify filter mixin

--- src/main/java/com/github/matt159/dws/mixin/mixins/common/gregtech/GT_Container_FilterMixin.java
+++ src/main/java/com/github/matt159/dws/mixin/mixins/common/gregtech/GT_Container_FilterMixin.java
@@ -1,30 +1,32 @@
 package com.github.matt159.dws.mixin.mixins.common.gregtech;
 
 import com.github.matt159.dws.util.Constants;
+import gregtech.api.gui.GT_Container;
+import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.common.gui.GT_Container_Filter;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
 
 @Mixin(GT_Container_Filter.class)
-public abstract class GT_Container_FilterMixin {
-    @ModifyConstant(method = "addSlots",
-                    constant = {
-                        @Constant(intValue = 8),
-                        @Constant(intValue = 18),
-                        @Constant(intValue = 26),
-                        @Constant(intValue = 35),
-                        @Constant(intValue = 44),
-                        @Constant(intValue = 52),
-                        @Constant(intValue = 62),
-                        @Constant(intValue = 80),
-                        @Constant(intValue = 98),
-                        @Constant(intValue = 116),
-                        @Constant(intValue = 134)
-                    },
-                    remap = false,
-                    require = 1)
-    private int modifySlotXOffsets(int constant) {
-        return constant + Constants.GENERAL_X_OFFSET;
+public abstract class GT_Container_FilterMixin extends GT_Container {
+    public GT_Container_FilterMixin(InventoryPlayer aPlayerInventory, IGregTechTileEntity aTileEntityInventory) {
+        super(aPlayerInventory, aTileEntityInventory);
+    }
+
+    @Redirect(method = "addSlots",
+              at = @At(value = "INVOKE",
+                       target = "Lgregtech/common/gui/GT_Container_Filter;addSlotToContainer(Lnet/minecraft/inventory/Slot;)Lnet/minecraft/inventory/Slot;"),
+              require = 23)
+    private Slot applySlotXOffset(GT_Container_Filter instance, Slot slot) {
+        slot.xDisplayPosition += Constants.GENERAL_X_OFFSET;
+
+        return this.addSlotToContainer(slot);
     }
 }
